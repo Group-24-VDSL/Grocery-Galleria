2c5be0a0ef3ffb4764a8371d405d03a3f027c2e4
Wathsala Vishwajith <>
2021-07-16 14:52:51 +0530

    fixed the show-pw button

--- js/template.js
+++ js/template.js
@@ -1,6 +1,5 @@
-function togglePassword() {
-    var element = document.getElementById("showpw"); 
-    element.classList.toggle("fa-eye fa-eye-slash");
-    var type = element.hasClass("fa-eye-slash") ? "text" : "password";
-    element.attr("type", type);
+function togglePassword() { 
+    $('#showpw').toggleClass('fa-eye fa-eye-slash');
+    var type = $('#showpw').hasClass("fa-eye-slash") ? "text" : "password";
+    $('#txtPassword').attr("type", type);
 }
