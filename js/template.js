dfcbfed440adcb22c58681659573f0d81daa79fc
Wathsala Vishwajith <wathsalavgithub.com>
2021-08-06 18:11:34 +0530

    mobile footer

--- js/template.js
+++ js/template.js
@@ -3,3 +3,18 @@ function togglePassword() {
     var type = $('#showpw').hasClass("fa-eye-slash") ? "text" : "password";
     $('#txtPassword').attr("type", type);
 }
+
+$('.ps-toggle--sidebar').on('click', function(e) {
+    e.preventDefault();
+    var url = $(this).attr('href');
+    $(this).toggleClass('active');
+    $(this)
+        .siblings('a')
+        .removeClass('active');
+    $(url).toggleClass('active');
+    $(url)
+        .siblings('.ps-panel--sidebar')
+        .removeClass('active');
+    $('.ps-site-overlay').toggleClass('active');
+    console.log("activated");
+});
