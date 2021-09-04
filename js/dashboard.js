99d5b182c8a207289a203260a05f7dfb28fb4fdc
Wathsala Vishwajith <>
2021-09-04 19:48:42 +0530

    sidebar dropdown menu

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,9 +3,26 @@ $(document).ready(function () {
         $(".sidebar").toggleClass("collapsed expand");
         $(".fa-bars").toggleClass("fa-rotate-90");
         $(".menu-expand").toggleClass("display-none");
+        $(".menu-expand-icon").toggleClass("display-none");
+        
     }
 
     $("#menu").click(function () {
         sidebartoggle();
     });
+
+    // $(".menu-expand-icon").on('click',function(){
+    //     dropdowntoggle();
+    // });
+
+    // function dropdowntoggle(){
+    //     $(this).siblings(".menu-dropdown-container").toggleClass("display-none");
+    //     $(".fa-menu-expand-icon").toggleClass("fa-caret-right fa-caret-down");
+    //     // content.toggleClass("display-none");
+    // }
+    $(".menu-expand-icon").on('click',function(){
+        $(this).parent().nextAll(".menu-dropdown-container").toggleClass("display-none")
+        $(this).children("i").toggleClass("fa-caret-right fa-caret-down");
+   });
+
 });
