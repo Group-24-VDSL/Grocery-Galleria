9b08d053adf7e9c096c14f8880bed41827f5d880
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-09-07 17:19:22 +0530

    add -Shop View Files

---
+++ public/js/Shop.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('#menu-bar');
+const navbar = document.querySelector('.navbar');
+const header = document.querySelector('.header-2');
+
+menu.addEventListener('click', function(){
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+});
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 50){
+      header.classList.add('active');
+  }else{
+      header.classList.remove('active');
+  }
+
+}
---
+++ views/shop.php
@@ -0,0 +1,187 @@
+<section class="product" id="product">
+
+    <h1 class="heading">Shop <span>products</span></h1>
+    <section id="uc">
+        <h1 class="subheading"><i class="fas fa-leaf"></i> Up <span>country</span></h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="./img/product-imgs/Pic914006.jpg" alt="">
+                <h3>Big Onion</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> Rs 67.00 <span>/</span> Kg </div>
+                <div class="quantity">
+                    <span>quantity : </span>
+                    <input type="number" id="quantity" name="quantity" min="0.5" max="10" step="0.5" value="0.5">
+                </div>
+                <a href="#" class="btn"><i class="fas fa-cart-plus"></i> add to cart</a>
+            </div>
+            <div class="box">
+                <img src="./img/product-imgs/Pic914007.jpg" alt="">
+                <h3>bitter gourd</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> Rs 67.00 <span>/</span> Kg </div>
+                <div class="quantity">
+                    <span>quantity : </span>
+                    <input type="number" id="quantity" name="quantity" min="0.5" max="10" step="0.5" value="0.5">
+                </div>
+                <a href="#" class="btn"><i class="fas fa-cart-plus"></i> add to cart</a>
+            </div>
+            <div class="box">
+                <img src="./img/product-imgs/Pic915007.jpg" alt="">
+                <h3>Carrot</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> Rs 67.00 <span>/</span> Kg </div>
+                <div class="quantity">
+                    <span>quantity : </span>
+                    <input type="number" id="quantity" name="quantity" min="0.5" max="10" step="0.5" value="0.5">
+                </div>
+                <a href="#" class="btn"><i class="fas fa-cart-plus"></i> add to cart</a>
+            </div>
+            <div class="box">
+                <img src="./img/product-imgs/Pic915013.jpg" alt="">
+                <h3>Potato</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> Rs 67.00 <span>/</span> Kg </div>
+                <div class="quantity">
+                    <span>quantity : </span>
+                    <input type="number" id="quantity" name="quantity" min="0.5" max="10" step="0.5" value="0.5">
+                </div>
+                <a href="#" class="btn"><i class="fas fa-cart-plus"></i> add to cart</a>
+            </div>
+        </div>
+
+    </section>
+    <section id="lc">
+        <h1 class="subheading"><i class="fas fa-leaf"></i> Low <span>country</span></h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="./img/product-imgs/Pic915016.jpg" alt="">
+                <h3>Tomato</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> Rs 67.00 <span>/</span> Kg </div>
+                <div class="quantity">
+                    <span>quantity : </span>
+                    <input type="number" id="quantity" name="quantity" min="0.5" max="10" step="0.5" value="0.5">
+                </div>
+                <a href="#" class="btn"><i class="fas fa-cart-plus"></i> add to cart</a>
+            </div>
+            <div class="box">
+                <img src="./img/product-imgs/Pic914029.jpg" alt="">
+                <h3>Lime</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> Rs 67.00 <span>/</span> Kg </div>
+                <div class="quantity">
+                    <span>quantity : </span>
+                    <input type="number" id="quantity" name="quantity" min="0.5" max="10" step="0.5" value="0.5">
+                </div>
+                <a href="#" class="btn"><i class="fas fa-cart-plus"></i> add to cart</a>
+            </div>
+            <div class="box">
+                <img src="./img/product-imgs/Pic914016.jpg" alt="">
+                <h3>Ginger</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> Rs 67.00 <span>/</span> Kg </div>
+                <div class="quantity">
+                    <span>quantity : </span>
+                    <input type="number" id="quantity" name="quantity" min="0.5" max="10" step="0.5" value="0.5">
+                </div>
+                <a href="#" class="btn"><i class="fas fa-cart-plus"></i> add to cart</a>
+            </div>
+            <div class="box">
+                <img src="./img/product-imgs/Pic914039.jpg" alt="">
+                <h3>Pumpkin</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> Rs 67.00 <span>/</span> Kg </div>
+                <div class="quantity">
+                    <span>quantity : </span>
+                    <input type="number" id="quantity" name="quantity" min="0.5" max="10" step="0.5" value="0.5">
+                </div>
+                <a href="#" class="btn"><i class="fas fa-cart-plus"></i> add to cart</a>
+            </div>
+            <div class="box">
+                <img src="./img/product-imgs/Pic915011.jpg" alt="">
+                <h3>Leeks</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> Rs 67.00 <span>/</span> Kg </div>
+                <div class="quantity">
+                    <span>quantity : </span>
+                    <input type="number" id="quantity" name="quantity" min="0.5" max="10" step="0.5" value="0.5">
+                </div>
+                <a href="#" class="btn"><i class="fas fa-cart-plus"></i> add to cart</a>
+            </div>
+            <div class="box">
+                <img src="./img/product-imgs/Pic915008.jpg" alt="">
+                <h3>Green Chillies</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price"> Rs 67.00 <span>/</span> Kg </div>
+                <div class="quantity">
+                    <span>quantity : </span>
+                    <input type="number" id="quantity" name="quantity" min="0.5" max="10" step="0.5" value="0.5">
+                </div>
+                <a href="#" class="btn"><i class="fas fa-cart-plus"></i> add to cart</a>
+            </div>
+        </div>
+
+    </section>
+</section>
+<!-- Product section end -->
