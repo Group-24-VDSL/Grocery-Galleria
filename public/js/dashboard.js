ce1ab9dc5792ab11fd0a456be0024f816841398f
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-09-10 02:08:12 +0530

    CSS and JS

---
+++ public/js/dashboard.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    function sidebartoggle() {
+        $(".sidebar").toggleClass("collapsed expand");
+        $(".fa-bars").toggleClass("fa-rotate-90");
+        $(".menu-expand").toggleClass("display-none");
+        $(".menu-expand-icon").toggleClass("display-none");
+        
+    }
+
+    $("#menu").click(function () {
+        sidebartoggle();
+    });
+
+    // $(".menu-expand-icon").on('click',function(){
+    //     dropdowntoggle();
+    // });
+
+    // function dropdowntoggle(){
+    //     $(this).siblings(".menu-dropdown-container").toggleClass("display-none");
+    //     $(".fa-menu-expand-icon").toggleClass("fa-caret-right fa-caret-down");
+    //     // content.toggleClass("display-none");
+    // }
+    $(".menu-expand-icon").on('click',function(){
+        $(this).parent().nextAll(".menu-dropdown-container").eq(0).toggleClass("display-none")
+        $(this).children("i").toggleClass("fa-caret-right fa-caret-down");
+   });
+
+});
---
+++ public/js/template.js
@@ -0,0 +1,37 @@
+$( function(){
+    function togglePassword() { 
+        $('#showpw').toggleClass('fa-eye fa-eye-slash');
+        var type = $('#showpw').hasClass("fa-eye-slash") ? "text" : "password";
+        $('#txtPassword').attr("type", type);
+    }
+    
+    $('.navigation-toggle').on('click', function(e) {
+        e.preventDefault();
+        var url = $(this).attr('href');
+        $(this).toggleClass('active');
+        $(this)
+            .siblings('a')
+            .removeClass('active');
+        $(url).toggleClass('active');
+        $(url)
+            .siblings('.navigation-toggle')
+            .removeClass('active');
+        $('.navigation-back-overlay').toggleClass('active');
+    });
+    
+    $('.navigation-back-overlay').on('click', function(e) {
+        e.preventDefault();
+        $('navigation-back-overlay').toggleClass('active');
+        $('.navigation-toggle')
+            .siblings('a')
+            .removeClass('active');
+        $('.navigation-back-overlay').toggleClass('active');
+        $('#menu-mobile').toggleClass('active');
+    
+    });
+    
+    $('.cart-mobile-content-item').on('click','.cart-mobile-item-remove',function(){
+        $(this).closest('.cart-mobile-content-item').remove();
+    });
+
+}); //document ready
