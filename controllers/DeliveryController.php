d90e4eb661c6407982b88a8c2f8b8d5d05bc46e6
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-09-15 19:35:51 +0530

    Delivery Rider logic

--- controllers/DeliveryController.php
+++ controllers/DeliveryController.php
@@ -2,22 +2,49 @@
 
 namespace app\controllers;
 
+use app\core\Application;
 use app\core\Controller;
 use app\core\Request;
 use app\models\Rider;
+use app\models\User;
 
 class DeliveryController extends Controller
 {
     public function addrider(Request $request)
     {
         $this->setLayout('dashboard-deli');
+        $user = new User();
         $rider = new Rider();
         if($request->isPost()){
+            $user->loadData($request->getBody());
+            $user->Role ='Rider'; //it is a rider
+            if ($user->validate() && $user->save()) {
+                //then we have to register the rider
+                //find the user-id to save as the rider-id
+                $temp = User::findOne(['Email' => $request->getBody()['Email']]);
+                $rider->RiderID = $temp->UserID;
+                if($rider->validate() && $rider->save()){
+                    Application::$app->session->setFlash('success', 'Rider Register Success');
+                    $this->setLayout('auth');
+                    exit;
+                }
+            }
 
         }
         return $this->render('delivery/add-rider', [
             'model' => $rider
         ]);
+    }
+
+    public function viewrider(Request $request)
+    {
+        $this->setLayout('dashboard-deli');
+        $id = $request->getBody()['id'];
+        $rider = Rider::findOne(['RiderID' => $id]);
+        return $this->render('delivery/view-rider',[
+            'model' => $rider
+            ]
+        );
     }
 
 }
