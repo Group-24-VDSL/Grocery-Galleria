d675c065f8d418f63e55b73d566065cc7743b5e3
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-09-20 17:26:48 +0530

    routes for add item staff dashboard

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use app\controllers\DeliveryController;
 use app\controllers\ShopController;
+use app\controllers\StaffController;
 use app\controllers\TestController;
 use app\core\Application;
 use app\controllers\SiteControllers;
@@ -23,9 +24,7 @@ $config = [
 $app = new Application(dirname(__DIR__), $config);
 
 $app->router->get('/',[SiteControllers::class,'welcome']);
-
 $app->router->get('/welcome',[SiteControllers::class,'welcome']);
-
 $app->router->get('/contact',[SiteControllers::class,'contact']);
 $app->router->post('/contact',[SiteControllers::class,'handleContact']);
 
@@ -33,15 +32,15 @@ $app->router->get('/login',[AuthController::class,'login']);
 $app->router->post('/login',[AuthController::class,'login']);
 $app->router->get('/register',[AuthController::class,'register']);
 $app->router->post('/register',[AuthController::class,'register']);
-
 $app->router->get('/logout',[AuthController::class,'logout']);
-
 $app->router->get('/profile',[AuthController::class,'profile']);
 
 $app->router->get('/gallery/shop',[ShopController::class,'showShop']);
 
 $app->router->get('/dashboard/staff/addrider',[DeliveryController::class,'addrider']);
 $app->router->get('/dashboard/staff/viewrider',[DeliveryController::class,'viewrider']);
+
+$app->router->get('/dashboard/staff/additem',[StaffController::class,'additem']);
 //for debugging purposes
 $app->router->get('/test',[TestController::class,'test']);
 
