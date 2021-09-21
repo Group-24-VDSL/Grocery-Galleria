d9a176f1ce93649d81eab936ad0bf75ddbbde90e
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-09-21 17:28:32 +0530

    Hidden Field and the number fields

--- core/form/InputFieldOnly.php
+++ core/form/InputFieldOnly.php
@@ -8,12 +8,15 @@ class InputFieldOnly
 {
     const TYPE_EMAIL = 'email';
     const TYPE_TEXT = 'text';
-    const TYPE_PASSWORD = 'password'; //add more types lists, checkboxes etc
+    const TYPE_PASSWORD = 'password';
+    const TYPE_NUMBER = 'number';
+    const TYPE_HIDDEN = 'hidden'; //add more types lists, checkboxes etc
 
     public string $type;
 
     public Model $model;
     public string $attribute;
+    public string $value;
     public $classes = [];
 
     public function __construct(Model $model, string $attribute,$classes=[]){
@@ -21,6 +24,7 @@ class InputFieldOnly
         $this->model = $model;
         $this->attribute = $attribute;
         $this->classes=$classes;
+        $this->value = $this->model->{$this->attribute};
     }
 
     public function passwordField()
@@ -35,13 +39,29 @@ class InputFieldOnly
         return $this;
     }
 
+    public function numberField(){
+        $this->type = self::TYPE_NUMBER;
+        return $this;
+    }
+
+    public function hiddenField()
+    {
+        $this->type = self::TYPE_HIDDEN;
+        return $this;
+    }
+
+    public function setValue(string $value){
+        $this->value = $value;
+        return $this;
+    }
+
     public function __toString()
     {
         return sprintf('<input type="%s" name="%s" style="%s" value="%s" class="%s" >',
             $this->type,
             $this->attribute,
             $this->model->hasError($this->attribute) ? "border: 1px solid red;" : '',
-            $this->model->{$this->attribute},
+            $this->value,
             $this->classes ? implode(" ",$this->classes): ""
         );
     }
