1e59665d80ccd2df39a48aa7edc01ff389c6170f
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-09-21 15:10:23 +0530

    Add item form for staff

--- views/staff/add-item.php
+++ views/staff/add-item.php
@@ -1,31 +1,29 @@
 
 <?php
-/** @var $model \app\models\Rider **/
-/** @var $form \core\form\Form */
+/** @var $model \app\models\Item **/
+/** @var $form app\core\form\Form */
 ?>
 <div class="main-content">
     <nav>
         <h1 class="main-header">Dashboard</h1>
     </nav>
     <div class="">
-        <h3>Add Rider</h3>
+        <h3>Add Item</h3>
         <div class="frm">
-            <?php $form = \app\core\form\Form::begin("","post");?>
+            <?php $form = \app\core\form\Form::begin("","post",[],"multipart/form-data");?>
             <div class="display-grid delivery-view">
                 <div class="delivery-view-details">
-                    ItemID 	 	 	 	UWeight
                     <div><?php echo $form->field($model,"Name");?></div>
-                    <div><?php echo $form->field($model,"ItemImage");?></div>
                     <div><?php echo $form->field($model,"Brand");?></div>
                     <div><?php echo $form->field($model,"UWeight");?></div>
-                    <div><?php echo $form->selectfield($model,"Unit",['kg'=>'Kg','g'=>'g','l'=>'liter'])?></div>
+                    <div><?php echo $form->selectfield($model,"Unit",['kg'=>'Kg','g'=>'g','l'=>'liter']);?></div>
                     <div><?php echo $form->field($model,"MRP");?></div>
-
-                </div   >
-                <div class="delivey-view-profile-pic">
-                    <div><label>Profile Picture:</label><img src='/img/placeholder-150.png'><input type=text name="" value="image.png" style="" class=""></div>
+                    <div><?php echo $form->field($model,"MaxCount");?></div>
                 </div>
-                <div class="submit-cancel"><button type="button" type="submit" class="btn btn-primary">Submit</button>
+                <div class="delivey-view-profile-pic">
+                    <div><label>Profile Picture:</label><img class="image-preview" src='/img/placeholder-150.png'><?php echo $form->inputfile($model,"ItemImage",['image-input'],"image/jpeg,image/png");?></div>
+                </div>
+                <div class="submit-cancel"><button type="submit" class="btn btn-primary">Submit</button>
                     <button type="button" class="btn btn-secondary">Cancel</button>
                 </div>
             </div>
