699827fd12d3981c90343b4ebf2c1f569c4cef7f
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-09-23 16:05:59 +0530

    Add item functionality in the controller

--- controllers/StaffController.php
+++ controllers/StaffController.php
@@ -2,13 +2,45 @@
 
 namespace app\controllers;
 
+use app\core\Application;
 use app\core\Controller;
+use app\core\Request;
+use app\models\Item;
 
 class StaffController extends Controller
 {
-    public function additems()
+    public function additem(Request $request)
     {
+        $item = new Item();
+        $this->setLayout("dashboard-staff");
+        if($request->isPost()){
+            var_dump($request->getBody());
+            var_dump($_FILES);
+            $item->loadData($request->getBody());
+            $itemimg = $request->loadFile("img/product-imgs/","ItemImage");
+            if($itemimg){
+                $item->ItemImage = $itemimg;
+                if($item->validate() && $item->save()){
+                    Application::$app->response->redirect("/");
+                }else{
+                    return $this->render("staff/add-item"
+                        ,[
+                            'model' => $item
+                        ]);
+                }
+            }else{
+                $item->addError("ItemImage","Something is wrong with the image, Try again");
+                return $this->render("staff/add-item"
+                    ,[
+                        'model' => $item
+                    ]);
+            }
 
+        }
+        return $this->render("staff/add-item"
+        ,[
+                'model' => $item
+            ]);
     }
 
     public function viewitems()
