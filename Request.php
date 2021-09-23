ccaca8e1d5d41fdeac030935c70aa9b68c50252b
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-09-24 00:20:27 +0530

    ability save the file if the file name is given

--- Request.php
+++ Request.php
@@ -42,21 +42,33 @@ class Request
         return $body;
     }
 
-    public function loadFile($path,$attribute)
+    public function loadFile($path,$attribute,$filename='')
     {
       if($this->method() === 'post'){
           $file = getimagesize($_FILES[$attribute]["tmp_name"]);
           if($file){
-              $target = $path.basename(filter_var($_FILES[$attribute]["name"],FILTER_SANITIZE_SPECIAL_CHARS));
-              echo $target;
-              if(!file_exists($target)){
-                 if($_FILES[$attribute]["size"] < 500000 ){ //0.5MB
-                     if(in_array(strtolower(pathinfo($target,PATHINFO_EXTENSION)),["png","jpg"])) {
-                         if(move_uploaded_file($_FILES[$attribute]["tmp_name"], $target)) {
-                            return $target;
-                         }
-                     }
-                 }
+              if($filename !== ''){
+                  $target = $path.$filename.strtolower(pathinfo(basename(filter_var($_FILES[$attribute]["name"], FILTER_SANITIZE_SPECIAL_CHARS)), PATHINFO_EXTENSION));
+                  if (!file_exists($target)) {
+                      if ($_FILES[$attribute]["size"] < 500000) { //0.5MB
+                          if (in_array(strtolower(pathinfo(basename(filter_var($_FILES[$attribute]["name"], FILTER_SANITIZE_SPECIAL_CHARS)), PATHINFO_EXTENSION)), ["png", "jpg"])) {
+                              if (move_uploaded_file($_FILES[$attribute]["tmp_name"], $target)) {
+                                  return $target;
+                              }
+                          }
+                      }
+                  }
+              }else {
+                  $target = $path . basename(filter_var($_FILES[$attribute]["name"], FILTER_SANITIZE_SPECIAL_CHARS));
+                  if (!file_exists($target)) {
+                      if ($_FILES[$attribute]["size"] < 500000) { //0.5MB
+                          if (in_array(strtolower(pathinfo($target, PATHINFO_EXTENSION)), ["png", "jpg"])) {
+                              if (move_uploaded_file($_FILES[$attribute]["tmp_name"], $target)) {
+                                  return $target;
+                              }
+                          }
+                      }
+                  }
               }
           }
       }
