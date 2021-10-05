5d3887209fe55df45ff8e10d8704800dc74f2e83
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-10-05 20:46:29 +0530

    Rider Registration Form

--- views/delivery/add-rider.php
+++ views/delivery/add-rider.php
@@ -14,15 +14,16 @@
                     <div class="delivery-view-details">
                         <div><?php echo $form->field($model,"Name");?></div>
                         <div><?php echo $form->field($model,"Address");?></div>
-                        <div><?php echo $form->field($model,"Email");?></div>
-                        <div><?php echo $form->field($model,"ContactNo");?></div>
+                        <div><?php echo $form->field($model,"Email")->emailField();?></div>
+                        <div><?php echo $form->field($model,"ContactNo")?></div>
                         <div><?php echo $form->field($model,"NIC");?></div>
+                        <div><?php echo  $form->selectfield($model,"RiderType",['0'=>'Bike','1'=>'Threewheel']);?></div>
                     </div>
                     <div class="delivey-view-profile-pic">
                         <div><label>Profile Picture:</label><img class="image-preview" src='/img/placeholder-150.png'><?php echo $form->inputfile($model,"ProfilePic",['image-input'],"image/jpeg,image/png");?></div>
                     </div>
                     <div class="submit-cancel"><button type="submit" type="submit" class="btn btn-primary">Submit</button>
-                        <button type="button" class="btn btn-secondary">Cancel</button>
+                        <button type="reset" class="btn btn-secondary">Cancel</button>
                     </div>
                 </div>
             <?php \app\core\form\Form::end()?>
