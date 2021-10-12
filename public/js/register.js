00bb66d3c43e35ed79f6f0386e78793285b5d33c
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-10-13 01:31:21 +0530

    Google maps intergration

--- public/js/register.js
+++ public/js/register.js
@@ -1,39 +1,29 @@
-$(function (){
+$(function (listener){
     // Create the script tag, set the appropriate attributes
-    var script = document.createElement('script');
+    let script = document.createElement('script');
     script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAwYJrYLyEaQGRUYEnh10GS5luyYnt2a5U&callback=initMap';
     script.async = true;
 
-    // let map;
-    // // Attach your callback function to the `window` object
-    // window.initMap = function() {
-    //     map = new google.maps.Map(document.getElementById("map"), {
-    //         center: { lat: -34.397, lng: 150.644 },
-    //         zoom: 8,
-    //     });
-    // };
-
-
-
-
     let map, infoWindow;
+    let inputfield = document.getElementsByName('Location')[0];
 
     window.initMap = function () {
+
         map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: 6.9271, lng: 79.8612 },
             zoom: 8,
         });
         infoWindow = new google.maps.InfoWindow();
 
-        var myMarker = new google.maps.Marker({
+        let myMarker = new google.maps.Marker({
             position: new google.maps.LatLng(6.9271, 79.8612),
             draggable: true
         });
 
         myMarker.setMap(map);
 
-        const locationButton = document.createElement("button");
-
+        const locationButton = document.createElement("button",);
+        locationButton.setAttribute('type','button');
         locationButton.textContent = "Find your Location";
         locationButton.classList.add("custom-map-control-button");
         map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
@@ -46,11 +36,9 @@ $(function (){
                             lat: position.coords.latitude,
                             lng: position.coords.longitude,
                         };
-
-                        infoWindow.setPosition(pos);
-                        infoWindow.setContent("Location found.");
-                        infoWindow.open(map);
+                        myMarker.setPosition(pos);
                         map.setCenter(pos);
+                        inputfield.setAttribute('value',JSON.stringify(pos));
                     },
                     () => {
                         handleLocationError(true, infoWindow, map.getCenter());
@@ -61,17 +49,24 @@ $(function (){
                 handleLocationError(false, infoWindow, map.getCenter());
             }
         });
+
+        google.maps.event.addListener(myMarker, 'dragend', function () {
+            const pos = this.getPosition();
+            inputfield.setAttribute('value',JSON.stringify(pos));
+        });
+
+
+        window.handleLocationError = function (browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(
+                browserHasGeolocation
+                    ? "Error: The Geolocation service failed."
+                    : "Error: Your browser doesn't support geolocation."
+            );
+            infoWindow.open(map);
+        }
     }
 
-    window.handleLocationError = function (browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-            browserHasGeolocation
-                ? "Error: The Geolocation service failed."
-                : "Error: Your browser doesn't support geolocation."
-        );
-        infoWindow.open(map);
-    }
 
     // Append the 'script' element to 'head'
     document.head.appendChild(script);
