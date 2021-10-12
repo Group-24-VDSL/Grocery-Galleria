21c72288e6568ef81d22c9e18a22b517bb49eed2
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-10-13 01:32:25 +0530

    Registration routes

--- public/index.php
+++ public/index.php
@@ -28,10 +28,12 @@ $app->router->get('/welcome',[SiteControllers::class,'welcome']);
 $app->router->get('/contact',[SiteControllers::class,'contact']);
 $app->router->post('/contact',[SiteControllers::class,'handleContact']);
 
+$app->router->get('/verify',[AuthController::class,'verify']);
+
 $app->router->get('/login',[AuthController::class,'login']);
 $app->router->post('/login',[AuthController::class,'login']);
-$app->router->get('/customer/register',[AuthController::class,'register']);
-$app->router->post('/customer/register',[AuthController::class,'register']);
+$app->router->get('/customer/register',[\app\controllers\CustomerController::class,'customerRegister']);
+$app->router->post('/customer/register',[\app\controllers\CustomerController::class,'customerRegister']);
 $app->router->get('/shop/register',[AuthController::class,'register']);
 $app->router->post('/shop/register',[AuthController::class,'register']);
 $app->router->get('/logout',[AuthController::class,'logout']);
@@ -40,8 +42,8 @@ $app->router->get('/profile',[AuthController::class,'profile']);
 $app->router->get('/gallery/shop',[ShopController::class,'showshop']);
 $app->router->get('/gallery',[ShopController::class,'showgallery']);
 
-$app->router->get('/dashboard/delivery/addrider',[DeliveryController::class,'addrider']);
-$app->router->post('/dashboard/delivery/addrider',[DeliveryController::class,'addrider']);
+$app->router->get('/dashboard/delivery/addrider',[DeliveryController::class, 'riderRegister']);
+$app->router->post('/dashboard/delivery/addrider',[DeliveryController::class, 'riderRegister']);
 $app->router->get('/dashboard/delivery/viewriders',[DeliveryController::class,'viewriders']);
 $app->router->get('/dashboard/delivery/viewrider',[DeliveryController::class,'viewrider']);
 $app->router->get('/dashboard/delivery/vieworders',[DeliveryController::class,'vieworders']);
