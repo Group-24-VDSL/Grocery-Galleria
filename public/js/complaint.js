5565925baa5ca40d6ed2b9630c8d04bf205c09ac
Sanduni <2019cs154@stu.ucsc.lk>
2021-10-13 00:30:59 +0530

    create popup complaint description

---
+++ public/js/complaint.js
@@ -0,0 +1,11 @@
+$(function(){
+  $(".btn-description").on("click",function(){
+    // $(this).closest("div.description").show();
+    $(this).siblings('.description').show();
+  });
+  
+  $(".close").on("click",function(){
+    // $(this).closest("div.description").show();
+    $(this).closest('.description').hide();
+  });
+});
