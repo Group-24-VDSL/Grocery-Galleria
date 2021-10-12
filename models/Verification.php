0fdf80fccaa21f3379b94e90adcd887cc2f84c84
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-10-13 01:31:40 +0530

    verification done

---
+++ models/Verification.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\models;
+
+use app\core\db\DBModel;
+
+class Verification extends DBModel
+{
+    public int $UserID = 0;
+    public string $VerificationCode = '';
+    public string $UniqueID = '';
+
+    public static function tableName(): string
+    {
+        return 'verification';
+    }
+
+    public function attributes(): array
+    {
+        return ['UserID', 'VerificationCode', 'UniqueID'];
+    }
+
+    public static function primaryKey(): string
+    {
+        return 'UserID';
+    }
+
+    public function rules(): array
+    {
+        return [
+            'UserID' => [[self::RULE_UNIQUE,'class'=> self::class]],
+            'VerificationCode' => [[self::RULE_UNIQUE,'class'=> self::class]],
+            'UniqueID' => [[self::RULE_UNIQUE,'class'=> self::class]]
+        ];
+    }
+
+    public function jsonarray(): array
+    {
+        return [];
+    }
+}
