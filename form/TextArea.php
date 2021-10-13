ac6d83463cf7352c2cfcb513f937ac04e338aed5
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-10-13 14:15:10 +0530

    setClasses function for text area

--- form/TextArea.php
+++ form/TextArea.php
@@ -4,14 +4,19 @@ namespace app\core\form;
 
 class TextArea extends BaseField
 {
-
+    public array $classes;
+    public function setClass($classes = [])
+    {
+        $this->classes=$classes;
+    }
     public function renderInput(): string
     {
         return sprintf(
-            '<textarea name="%s" style="%s">%s</textarea>
+            '<textarea name="%s"  style="%s" class="%s" >%s</textarea>
 <div><small style="color: red">%s</small></div>',
             $this->attribute,
         $this->model->hasError($this->attribute) ? "border: 1px solid red;" : '',
+            $this->classes? implode(' ',$this->classes): '',
             $this->model->{$this->attribute},
             $this->model->getFirstError($this->attribute)
         );
