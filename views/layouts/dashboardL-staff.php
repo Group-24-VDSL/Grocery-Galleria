256bcf42250f68826c2f4cdac2b5464922c9e591
Dilshan98-code <dilshanthenuka9@gmail.com>
2021-10-13 11:53:00 +0530

    Staf dashboard layout added.

--- views/layouts/dashboardL-staff.php
+++ views/layouts/dashboardL-staff.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="/css/all.css" />
     <link rel="stylesheet" href="/css/fonts.css" />
     <link rel="stylesheet" href="/css/dashboardStyleStaff.css">
+    <link rel="stylesheet" href="/css/template.css">
 <!--    <link rel="stylesheet" href="/css/boxicons.min.css" />-->
     <link
             href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css"
