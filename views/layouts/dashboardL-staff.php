7db3c9c5f1a6a2117b947a944507d385a89f6b01
Dilshan98-code <dilshanthenuka9@gmail.com>
2021-10-14 14:06:29 +0530

    Jquery configured.

--- views/layouts/dashboardL-staff.php
+++ views/layouts/dashboardL-staff.php
@@ -14,9 +14,9 @@
             href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css"
             rel="stylesheet"
     />
-    <!--Javascript-->
-    <!--    <script src="/js/jquery.min.js"></script>-->
-    <!--    <script src="/js/template.js"></script>-->
+    Javascript
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/template.js"></script>
     <title>Dashboard-Shop</title>
 </head>
 <?php include_once("utils/sessions.php"); ?>
