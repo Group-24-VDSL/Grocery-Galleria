d315d079bf541dad0d98dce451fdce3981ce0032
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-10-16 02:26:04 +0530

    Update the service worker

--- views/layouts/utils/pwa.php
+++ views/layouts/utils/pwa.php
@@ -1,6 +1,2 @@
 <link rel="manifest" href="/manifest.json">
-<script type="module">
-    import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
-    const el = document.createElement('pwa-update');
-    document.body.appendChild(el);
-</script>
+<script type="module" src="pwabuilder-sw-register.js"></script>
