859ee5ae60de4bda0e7a43e10fdd4ae24b9ed235
Sanduni <2019cs154@stu.ucsc.lk>
2021-10-16 18:02:05 +0530

    add register.php for staff

---
+++ views/staff/register.php
@@ -0,0 +1,53 @@
+<section class="register" id="register">
+    <h1 class="heading">Registration <span>Form</span> Staff</h1>
+    <form action="" id="Regform">
+        <div class="inputBox">
+            <label for="Name"><i class="fas fa-edit"></i>Name</label>
+            <input type="text" placeholder="Name" id="name">
+            <i class="iconSE fas fa-check-circle"></i>
+            <i class="iconSE fas fa-exclamation-circle"></i>
+            <small></small>
+        </div>
+
+        <div class="inputBox">
+            <label for="Address"><i class="fas fa-home"></i>Address</label>
+            <input type="text" placeholder="Eg: No:56/B Example Rd, Example" id="address">
+            <i class="iconSE fas fa-check-circle"></i>
+            <i class="iconSE fas fa-exclamation-circle"></i>
+            <small></small>
+        </div>
+        <div class="inputBox">
+            <label for="Email"><i class="fas fa-envelope"></i>Email</label>
+            <input type="email" placeholder="example@mail.com" id="email">
+            <i class="iconSE fas fa-check-circle"></i>
+            <i class="iconSE fas fa-exclamation-circle"></i>
+            <small></small>
+        </div>
+        <div class="inputBox">
+            <label for="contact"><i class="fas fa-phone"></i>Contact No</label>
+            <input type="text" placeholder="011 2567890" id="contact">
+            <i class="iconSE fas fa-check-circle"></i>
+            <i class="iconSE fas fa-exclamation-circle"></i>
+            <small></small>
+        </div>
+        <div class="inputBox">
+            <label for="Password"><i class="fas fa-key"></i>Password</label>
+            <input type="password" placeholder="password" id="password">
+            <i class="iconSE fas fa-check-circle"></i>
+            <i class="iconSE fas fa-exclamation-circle"></i>
+            <small></small>
+        </div>
+        <div class="inputBox">
+            <label for="PasswordR"><i class="fas fa-key"></i>Re-enter password</label>
+            <input type="password" placeholder="Re-enter password" id="passwordR">
+            <i class="iconSE fas fa-check-circle"></i>
+            <i class="iconSE fas fa-exclamation-circle"></i>
+            <small></small>
+        </div>
+        <div class="inputBox"></div>
+        <div class="btn-align">
+            <button id="deny" type="submit" class="btn submit deny">Deny Registration</button>
+            <button id="accept" type="submit" class="btn submit">Accept Registration</button>
+        </div>
+    </form>
+</section>
