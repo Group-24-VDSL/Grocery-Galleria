1dc9abbdf1bd09c7d77a6191b057a4a3a65335d5
Dilshan98-code <dilshanthenuka9@gmail.com>
2021-10-18 11:58:01 +0530

    Auth fix.

--- public/js/Auth.js
+++ public/js/Auth.js
@@ -1,20 +1,17 @@
- $(function(){const inputs = document.querySelectorAll('.input')
+const inputs = document.querySelectorAll('.input')
 
  function focusFunc(){
-   let parent = this.parentNode.parentNode;
-   parent.classList.add('focus');
+     let parent = this.parentNode.parentNode;
+     parent.classList.add('focus');
  }
  function blurFunc(){
-   let parent = this.parentNode.parentNode;
-   if(this.value == ""){
-    parent.classList.remove('focus');
-   }
+     let parent = this.parentNode.parentNode;
+     if(this.value == ""){
+         parent.classList.remove('focus');
+     }
  }
 
  inputs.forEach(input =>{
-   input.addEventListener('focus',focusFunc); 
-   input.addEventListener('blur',blurFunc); 
- });
-
-
+     input.addEventListener('focus',focusFunc);
+     input.addEventListener('blur',blurFunc);
  });
