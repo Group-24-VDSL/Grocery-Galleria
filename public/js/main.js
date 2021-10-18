82819208a2b92cbb5e50197533e8d8a4ea730923
Dilshan98-code <dilshanthenuka9@gmail.com>
2021-10-18 12:06:14 +0530

    Profile tab js.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('#menu-bar');
 const navbar = document.querySelector('.navbar');
 const header = document.querySelector('.header-2');
+const tabBtns = document.querySelectorAll(".tab-btn");
+const infoDivs = document.querySelectorAll(".info-div");
 
 menu.addEventListener('click', function(){
   menu.classList.toggle('fa-times');
@@ -10,10 +12,37 @@ window.onscroll = () =>{
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 
-  if(window.scrollY > 100){
+  if(window.scrollY > 10){
       header.classList.add('active');
   }else{
       header.classList.remove('active');
   }
-
 }
+
+let currentTab = tabBtns[0]; // initial state
+let currentInfo = infoDivs[0]; // initial state
+
+tabBtns[0].classList.toggle("show-text");
+infoDivs[0].classList.toggle("show-text");
+
+tabBtns.forEach(function (tabBtn) {
+    tabBtn.addEventListener("click", function () {
+        if (currentTab !== tabBtn) {
+            currentTab.classList.remove("show-text");
+            currentInfo.classList.remove("show-text");
+        } else {
+            currentTab.classList.toggle("show-text");
+            currentInfo.classList.toggle("show-text");
+        }
+
+        infoDivs.forEach(function (infoDiv) {
+            if (infoDiv.id == tabBtn.parentNode.className) {
+                currentTab = tabBtn;
+                currentInfo = infoDiv;
+                currentTab.classList.toggle("show-text");
+                currentInfo.classList.toggle("show-text");
+            }
+            // infoDiv.classList.toggle("show-text");
+        });
+    });
+});
