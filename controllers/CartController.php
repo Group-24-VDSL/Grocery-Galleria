99849f48461071c7ada27cb7495481598114f5ed
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-11-26 00:19:06 +0530

    Finalizing the cart with remove function and the update function.

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -5,78 +5,78 @@ namespace app\controllers;
 use app\core\Controller;
 use app\core\Request;
 use app\core\Response;
-use app\models\Shop;
 use app\models\ShopItem;
 use app\models\TemporaryCart;
 
 
 class CartController extends Controller
 {
-    public function cart(Request $request,Response $response)
+    public function cart(Request $request, Response $response)
     {
         $this->setLayout('cart');
-        $items = TemporaryCart::findAll(array_slice($request->getBody(),1,null,true));
+        $items = TemporaryCart::findAll(array_slice($request->getBody(), 1, null, true));
         $total = 0;
         $shopitems = [];
-        foreach($items as $item){
-            $shopitem = ShopItem::findOne(['ItemID'=>$item->ItemID,'ShopID'=>$item->ShopID]);
-            $shopitems[$item->ShopID][$item->ItemID]=[$shopitem,$item];
+        foreach ($items as $item) {
+            $shopitem = ShopItem::findOne(['ItemID' => $item->ItemID, 'ShopID' => $item->ShopID]);
+            $shopitems[$item->ShopID][$item->ItemID] = [$shopitem, $item];
             $total += $shopitem->UnitPrice * $item->Quantity;
         }
 
-        return $this->render('customer/cart',['items'=>$items,'total'=>$total,'shopitems' =>$shopitems]);
+        return $this->render('customer/cart', ['items' => $items, 'total' => $total, 'shopitems' => $shopitems]);
     }
 
-    public function getCart(Request $request,Response $response) // get all items from DB
+    public function getCart(Request $request, Response $response) // get all items from DB
     {
-        $items = TemporaryCart::findAll(array_slice($request->getBody(),1,null,true));
+        $items = TemporaryCart::findAll(array_slice($request->getBody(), 1, null, true));
         $total = 0;
-        foreach($items as $item){
-            $shopitem = ShopItem::findOne(['ItemID'=>$item->ItemID,'ShopID'=>$item->ShopID]);
+        foreach ($items as $item) {
+            $shopitem = ShopItem::findOne(['ItemID' => $item->ItemID, 'ShopID' => $item->ShopID]);
             $total += $shopitem->UnitPrice * $item->Quantity;
         }
-        return $response->json(['items'=>$items,'total'=>$total]);
+        return $response->json(['items' => $items, 'total' => $total]);
     }
 
-    public function addToCart(Request $request,Response $response)
+    public function addToCart(Request $request, Response $response)
     {
-        if($request->isPost()){
-            $json = $request->getJson();
-            if($json){
-                $tempcart = new TemporaryCart();
-                $tempcart->loadData($json);
-                $checktemp = TemporaryCart::findOne(["ItemID"=>$tempcart->ItemID,"ShopID"=>$tempcart->ShopID,"CustomerID"=>$tempcart->CustomerID]);
-                if($checktemp) { //there exists such item
+        $json = $request->getJson();
+        if ($json) {
+            $tempcart = new TemporaryCart();
+            $tempcart->loadData($json);
+            $checktemp = TemporaryCart::findOne(["ItemID" => $tempcart->ItemID, "ShopID" => $tempcart->ShopID, "CustomerID" => $tempcart->CustomerID]);
+            if ($request->isPost()) {
+                if ($checktemp) { //there exists such item
                     $newQuantity = $tempcart->Quantity + $checktemp->Quantity;
                     $tempcart->Quantity = $newQuantity;
-                    if ($tempcart->validate() && $tempcart->update()) {
+                    if ($tempcart->validate('update') && $tempcart->update()) {
                         return $response->json('{"success":"ok"}');
-                    }else{
-                        return $response->json('{"success":"fail"}');
                     }
-                }else{
+                } else {
                     if ($tempcart->validate() && $tempcart->save()) {
                         return json_encode('{"success":"ok"}');
-                    } else {
-                        return $response->json('{"success":"fail"}');
+                    }
+                }
+                return $response->json('{"success":"fail"}');
+            } elseif($request->isPatch()) {
+                if ($checktemp) { //there exists such item
+                    $tempcart->loadData($checktemp);
+                    $tempcart->Quantity = $json['Quantity'];
+                    if ($tempcart->validate('update') && $tempcart->update()) {
+                        return $response->json('{"success":"ok"}');
                     }
                 }
             }
-            return $response->json('{"success":"fail"}');
-
-        }else{
-            return $response->json('{"success":"fail"}');
         }
+        return $response->json('{"success":"fail"}');
     }
 
-    public function deleteFromCart(Request $request,Response $response)
+    public function deleteFromCart(Request $request, Response $response)
     {
-        if($request->isPost()){
-            $response->setContentTypeJSON();
+        if ($request->isPost()) {
             $json = $request->getJson();
-            if($json) {
+            if ($json) {
                 $status = TemporaryCart::delete($json);
-                if($status){
+                if ($status) {
                     return $response->json('{"success":"ok"}');
                 }
             }
