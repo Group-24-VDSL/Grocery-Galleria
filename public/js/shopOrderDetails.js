31e54fd9516106430384dbe4f1402dacb67009b6
Sanduni <2019cs154@stu.ucsc.lk>
2021-11-28 15:33:26 +0530

    remove onclick function when mark as completed button click

--- public/js/shopOrderDetails.js
+++ public/js/shopOrderDetails.js
@@ -1,24 +1,13 @@
-// document.getElementById('complete').onclick = function() {
-//     var button = document.getElementById('complete');
-//     document.getElementById('status').innerHTML = 'Order Completed !!!';
-//     //document.getElementById('status').style.color = "Green";
-//
-//     button.style.color = "Green" ;
-//     button.style.backgroundColor = "White";
-// }
-//
-// function hide(id1) {
-//     document.getElementById(id1).style.display = "none";
-// }
-// function show(id2){
-//     document.getElementById(id2).style.display = "block";
-//
-// }
+const host = window.location.origin; //http://domainname
+const URLMarkCompleted= host + "/api/shopOrderStatus";
 
-function buttonStatus() {
-    var x = document.getElementById("not-complete");
-    var y = document.getElementById("completed");
+function markCompleted() {
+    let x = document.getElementById("not-complete");
+    let y = document.getElementById("completed");
 
     x.style.display = "none";
     y.style.display = "block";
+
+    //  window.history.back();
 }
+
