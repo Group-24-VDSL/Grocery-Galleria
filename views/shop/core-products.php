312077114909b212782140dc2aaaeebafbd4c23e
Sanduni <2019cs154@stu.ucsc.lk>
2021-12-03 03:56:27 +0530

    functional shop item update

--- views/shop/core-products.php
+++ views/shop/core-products.php
@@ -5,8 +5,10 @@
 
 <link rel="stylesheet" href="/css/shop-items.css">
 <script src="/js/dashboard-shop.js" defer></script>
+<link rel="stylesheet" href="/css/dashboardStyle.css">
+<link rel="stylesheet" href="/css/dashboardStyleStaff.css">
 
-<div class="core">
+<div class="core" style="height: height: 580px">
     <h1 class="heading">Ongoing <span>Products</span></h1>
     <div class="container-items">
 
@@ -18,13 +20,13 @@
                 <th>Item Image</th>
                 <th>Item Name</th>
                 <th>Brand</th>
-                <th>U/Price</th>
                 <th>U/Weight</th>
                 <th>Max Price</th>
                 <th>U/Price</th>
-                <th>Max Price</th>
+                <th>Unit Price</th>
                 <th>Stock</th>
                 <th>enable</th>
+                <th></th>
             </tr>
             </thead>
             <?php $form = \app\core\form\Form::begin("","post",'',[""]); ?>
@@ -34,215 +36,104 @@
             </tbody>
             <?php \app\core\form\Form::end(); ?>
         </table>
-<!--        <div class="ongoing-items scroller">-->
-<!--            <ul class="item">-->
-<!--                <li class="item-img">-->
-<!--                    <img src="/views/shop/img/Pic915013.jpg" alt="" />-->
-<!--                </li>-->
-<!--                <li class="item-name">Potato</li>-->
-<!--                <li class="item-mrp">67</li>-->
-<!---->
-<!--                <li class="item-unit">Kg</li>-->
-<!---->
-<!--                <li class="item-uprice">-->
-<!--                    <input-->
-<!--                            type="number"-->
-<!--                            min="10"-->
-<!--                            max="200"-->
-<!--                            value="10"-->
-<!--                            id="unitprice"-->
-<!--                            name="unitprice"-->
-<!--                    />-->
-<!--                </li>-->
-<!--                <li class="item-uStock">-->
-<!--                    <input-->
-<!--                            type="number"-->
-<!--                            min="10"-->
-<!--                            max="200"-->
-<!--                            value="10"-->
-<!--                            id="currentStock"-->
-<!--                            name="currentStock"-->
-<!--                    />-->
-<!--                </li>-->
-<!--                <li class="item-ubutton">-->
-<!--                    <button class="btn-item" type="submit">Update</button>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--            <ul class="item">-->
-<!--                <li class="item-img">-->
-<!--                    <img src="/views/shop/img/Pic914005.jpg" alt="" />-->
-<!--                </li>-->
-<!--                <li class="item-name">Big Onion</li>-->
-<!--                <li class="item-mrp">67</li>-->
-<!--                <li class="item-uprice">81</li>-->
-<!--                <li class="item-unit">Kg</li>-->
-<!--                <li class="item-cStock">-->
-<!--                    <label for="currentStock">10</label>-->
-<!--                </li>-->
-<!--                <li class="item-uStock">-->
-<!--                    <input-->
-<!--                            type="number"-->
-<!--                            min="10"-->
-<!--                            max="200"-->
-<!--                            value="10"-->
-<!--                            id="currentStock"-->
-<!--                            name="currentStock"-->
-<!--                    />-->
-<!--                </li>-->
-<!--                <li class="item-ubutton">-->
-<!--                    <button class="btn-item" type="submit">Update</button>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--            <ul class="item">-->
-<!--                <li class="item-img">-->
-<!--                    <img src="/views/shop/img/Pic914007.jpg" alt="" />-->
-<!--                </li>-->
-<!--                <li class="item-name">Green Chillies</li>-->
-<!--                <li class="item-mrp">67</li>-->
-<!--                <li class="item-uprice">81</li>-->
-<!--                <li class="item-unit">Kg</li>-->
-<!--                <li class="item-cStock">-->
-<!--                    <label for="currentStock">10</label>-->
-<!--                </li>-->
-<!--                <li class="item-uStock">-->
-<!--                    <input-->
-<!--                            type="number"-->
-<!--                            min="10"-->
-<!--                            max="200"-->
-<!--                            value="10"-->
-<!--                            id="currentStock"-->
-<!--                            name="currentStock"-->
-<!--                    />-->
-<!--                </li>-->
-<!--                <li class="item-ubutton">-->
-<!--                    <button class="btn-item" type="submit">Update</button>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--            <ul class="item">-->
-<!--                <li class="item-img">-->
-<!--                    <img src="/views/shop/img/Pic914015.jpg" alt="" />-->
-<!--                </li>-->
-<!--                <li class="item-name">Garlic</li>-->
-<!--                <li class="item-mrp">67</li>-->
-<!--                <li class="item-uprice">81</li>-->
-<!--                <li class="item-unit">Kg</li>-->
-<!--                <li class="item-cStock">-->
-<!--                    <label for="currentStock">10</label>-->
-<!--                </li>-->
-<!--                <li class="item-uStock">-->
-<!--                    <input-->
-<!--                            type="number"-->
-<!--                            min="10"-->
-<!--                            max="200"-->
-<!--                            value="10"-->
-<!--                            id="currentStock"-->
-<!--                            name="currentStock"-->
-<!--                    />-->
-<!--                </li>-->
-<!--                <li class="item-ubutton">-->
-<!--                    <button class="btn-item" type="submit">Update</button>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--            <ul class="item">-->
-<!--                <li class="item-img">-->
-<!--                    <img src="/views/shop/img/Pic915016.jpg" alt="" />-->
-<!--                </li>-->
-<!--                <li class="item-name">Tomatoe</li>-->
-<!--                <li class="item-mrp">67</li>-->
-<!--                <li class="item-uprice">81</li>-->
-<!--                <li class="item-unit">Kg</li>-->
-<!--                <li class="item-cStock">-->
-<!--                    <label for="currentStock">10</label>-->
-<!--                </li>-->
-<!--                <li class="item-uStock">-->
-<!--                    <input-->
-<!--                            type="number"-->
-<!--                            min="10"-->
-<!--                            max="200"-->
-<!--                            value="10"-->
-<!--                            id="currentStock"-->
-<!--                            name="currentStock"-->
-<!--                    />-->
-<!--                </li>-->
-<!--                <li class="item-ubutton">-->
-<!--                    <button class="btn-item" type="submit">Update</button>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--            <ul class="item">-->
-<!--                <li class="item-img">-->
-<!--                    <img src="/views/shop/img/Pic915008.jpg" alt="" />-->
-<!--                </li>-->
-<!--                <li class="item-name">Tomatoe</li>-->
-<!--                <li class="item-mrp">67</li>-->
-<!--                <li class="item-uprice">81</li>-->
-<!--                <li class="item-unit">Kg</li>-->
-<!--                <li class="item-cStock">-->
-<!--                    <label for="currentStock">10</label>-->
-<!--                </li>-->
-<!--                <li class="item-uStock">-->
-<!--                    <input-->
-<!--                            type="number"-->
-<!--                            min="10"-->
-<!--                            max="200"-->
-<!--                            value="10"-->
-<!--                            id="currentStock"-->
-<!--                            name="currentStock"-->
-<!--                    />-->
-<!--                </li>-->
-<!--                <li class="item-ubutton">-->
-<!--                    <button class="btn-item" type="submit">Update</button>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--            <ul class="item">-->
-<!--                <li class="item-img">-->
-<!--                    <img src="/views/shop/img/Pic914039.jpg" alt="" />-->
-<!--                </li>-->
-<!--                <li class="item-name">Pumpkin</li>-->
-<!--                <li class="item-mrp">67</li>-->
-<!--                <li class="item-uprice">81</li>-->
-<!--                <li class="item-unit">Kg</li>-->
-<!--                <li class="item-cStock">-->
-<!--                    <label for="currentStock">10</label>-->
-<!--                </li>-->
-<!--                <li class="item-uStock">-->
-<!--                    <input-->
-<!--                            type="number"-->
-<!--                            min="10"-->
-<!--                            max="200"-->
-<!--                            value="10"-->
-<!--                            id="currentStock"-->
-<!--                            name="currentStock"-->
-<!--                    />-->
-<!--                </li>-->
-<!--                <li class="item-ubutton">-->
-<!--                    <button class="btn-item" type="submit">Update</button>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--            <ul class="item">-->
-<!--                <li class="item-img">-->
-<!--                    <img src="/views/shop/img/Pic914012.jpg" alt="" />-->
-<!--                </li>-->
-<!--                <li class="item-name">Tomatoe</li>-->
-<!--                <li class="item-mrp">67</li>-->
-<!--                <li class="item-uprice">81</li>-->
-<!--                <li class="item-unit">Kg</li>-->
-<!--                <li class="item-cStock">-->
-<!--                    <label for="currentStock">10</label>-->
-<!--                </li>-->
-<!--                <li class="item-uStock">-->
-<!--                    <input-->
-<!--                            type="number"-->
-<!--                            min="10"-->
-<!--                            max="200"-->
-<!--                            value="10"-->
-<!--                            id="currentStock"-->
-<!--                            name="currentStock"-->
-<!--                    />-->
-<!--                </li>-->
-<!--                <li class="item-ubutton">-->
-<!--                    <button class="btn-item" type="submit">Update</button>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--        </div>-->
+
     </div>
 </div>
+
+<a name = "update">
+    <div class="core" id="Update">
+    <h1 class="heading">Update <span>Products</span></h1>
+
+    <div class="container-core">
+
+        <div class="form-details register">
+            <div id = "updateItem">
+
+            </div>
+            <?php $form = \app\core\form\Form::begin("","post","itemUpdate",[],"multipart/form-data",);?>
+
+            <input id="ItemID" name="ItemID" value="" hidden>
+            <div class="inputBox">
+                <label for="ID">
+                    <i class="fas fa-sort-amount-down"></i>
+                    <?php echo $model->labels()['ItemID']?>
+                </label>
+
+                <span class="fileds" id = "updateID">
+
+                </span>
+            </div>
+            <div class="inputBox">
+                <label for="Name">
+                    <i class="fas fa-edit"></i>
+                    <?php echo $model->labels()['Name']?>
+                </label>
+
+                <span class="fileds" id = "updateName">
+
+                </span>
+
+            </div>
+            <div class="inputBox">
+                <label for="Image">
+                    <i class="far fa-images"></i>
+                    <?php echo $model->labels()['Image']?>
+                </label>
+
+                <span class="image-box" >
+                    <img id="updateImage" >
+                </span>
+
+            </div>
+            <div class="inputBox">
+                <label for="Stock">
+                   <i class="fas fa-boxes"></i>
+                    <?php echo $model->labels()['Stock']?>
+                </label>
+                <?php echo $form->numberfieldonly($model,"Stock",10,10000,10);?>
+            </div>
+
+            <div class="inputBox">
+                <label for="Uprice">
+                    <i class="fas fa-coins"></i>
+                    <?php echo $model->labels()['UPrice']?>
+                </label>
+                <?php echo $form->numberfieldonly($model,"UnitPrice",10,10000,10);?>
+            </div>
+
+            <div class="inputBox">
+                <label for="Enable">
+                    <i class="far fa-flag"></i>
+                    <?php echo $model->labels()['Enable']?>
+                </label>
+
+                <div  id="bit00_3">
+                    <label class="switch">
+                        <input type="checkbox" id="checkbox1">
+                        <div class="slider round">
+                            <span class="on">Enable&nbsp&nbsp</span>
+                            <span class="off">&nbsp&nbspDisable</span>
+                        </div>
+                    </label>
+                </div>
+
+<!--                --><?php //echo $form->numberfieldonly($model,"Enabled",0,1,1);?>
+<!--                <span style="color: #003d2e">Enable --><?php //echo $form->fieldonly($model,"Enabled")->radioField()->setValue(1); ?><!--</span>-->
+<!---->
+<!--                <span style="color: red">Disable --><?php //echo $form->fieldonly($model,"Enabled")->radioField()->setValue(0); ?><!--</span>-->
+
+            </div>
+            <?php echo $form->fieldonly($model,"ShopID")->hiddenField();?>
+            <?php echo $form->fieldonly($model,"Enabled")->hiddenField();?>
+
+            <!--                <div class="inputBox"></div>-->
+            <div class="inputBox btn-div">
+                <button type="submit" class="btn update">Update</button>
+            </div>
+            <?php \app\core\form\Form::end()?>
+        </div>
+    </div>
+</div>
+
+</a>
+
