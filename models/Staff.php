ffb99892232a74bc79b46762602f61ceb248c98d
Dilshan98-code <dilshanthenuka9@gmail.com>
2021-12-02 19:53:15 +0530

    Password attributes removed.

--- models/Staff.php
+++ models/Staff.php
@@ -27,8 +27,6 @@ class Staff extends UserModel
     {
         return [
             'Email' => [self::RULE_EMAIL,self::RULE_REQUIRED,[self::RULE_UNIQUE,'class'=> self::class]], //check if the user class has the same email or not.
-            'Password' => [[self::RULE_MIN,'min' => 8]],
-            'ConfirmPassword' => [[self::RULE_MATCH,'match' => 'Password']],
             'Name' => [self::RULE_REQUIRED],
             'ContactNo'=>[self::RULE_REQUIRED,[self::RULE_MAX,'max' => 10],[self::RULE_MIN,'min' => 10]],
         ];
