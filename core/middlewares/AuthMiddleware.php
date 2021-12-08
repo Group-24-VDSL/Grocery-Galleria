b6791eccae0e542496c10ebd6b00f59dc8a5434f
Wathsala Vishwajith <64484342+wathsala-vishwajith@users.noreply.github.com>
2021-12-08 22:10:46 +0530

    Authentication middleware logic WIP

--- core/middlewares/AuthMiddleware.php
+++ core/middlewares/AuthMiddleware.php
@@ -8,7 +8,6 @@ use app\core\exceptions\ForbiddenException;
 class AuthMiddleware extends BaseMiddleware
 {
     public array $actions;
-
     /**
      * @param array $actions
      */
@@ -23,8 +22,13 @@ class AuthMiddleware extends BaseMiddleware
      */
     public function execute()
     {
+        Application::$app->logger->debug("Here");
         if(Application::isGuest()){
-            if(empty($this->actions) || in_array(Application::$app->controller->action,$this->actions )){ //checks the current action is in the actions array which are protected.
+            if(empty($this->actions) || !in_array(Application::$app->controller->action,$this->actions["Guest"] ) ){ //checks the current action is in the actions array which are not protected.
+                throw new ForbiddenException();
+            }
+        }else{
+            if(!in_array(Application::$app->controller->action,$this->actions["Common"] ) || !in_array(Application::$app->controller->action,$this->actions[Application::getUserRole()] ) ){ //checks the current action is in the actions array which are not protected.
                 throw new ForbiddenException();
             }
         }
