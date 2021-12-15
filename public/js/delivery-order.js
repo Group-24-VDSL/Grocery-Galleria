a30aa075a47bcb2ed13af085688ff15970b10698
Dilshan98-code <dilshanthenuka9@gmail.com>
2021-12-15 15:26:45 +0530

    new order render completed.

---
+++ public/js/delivery-order.js
@@ -0,0 +1,54 @@
+
+const host = window.location.origin; //http://domainname
+
+const newDeliveryURL = host+'/dashboard/delivery/newdelivery';
+const onDeliveryURL = host+'/dashboard/delivery/onDelivery';
+const pastDeliveryURL = host+'/dashboard/delivery/pastDelivery';
+
+const newTab = document.getElementById('new-tab');
+const onTab = document.getElementById('on-tab');
+const pastTab = document.getElementById('past-tab');
+
+$(document).ready(function (){
+    $('#new-tab').click(function (){
+        $('#newDelivery').removeClass('display-off');
+        $('#onDelivery').addClass('display-off');
+        $('#pastDelivery').addClass('display-off');
+        $('#new-delivery-table').empty();
+        $.getJSON(newDeliveryURL,function (deliveries){
+            console.log(deliveries);
+            deliveries.forEach(delivery=>{
+                const orderRow = document.createElement('tr');
+                orderRow.innerHTML = `
+                        <td>${delivery.OrderID}</td>
+                        <td>${delivery.OrderDate}</td>
+                        <td>${delivery.RecipientName}</td>
+                        <td>${delivery.Note}</td>
+                        <td>${delivery.RecipientContact}</td>
+                        <td>${delivery.DeliveryCost}</td>
+                        <td>${delivery.TotalCost}</td>
+                        <td> <a class="order-view" href="">
+                        <i class="fas fa-arrow-circle-right"></i></a></td>
+                `
+                $('#new-delivery-table').append(orderRow);
+            })
+        })
+    });
+    $('#on-tab').click(function (){
+        $('#newDelivery').addClass('display-off');
+        $('#onDelivery').removeClass('display-off');
+        $('#pastDelivery').addClass('display-off');
+        $('#on-delivery-table').empty();
+
+    });
+    $('#past-tab').click(function (){
+        $('#newDelivery').addClass('display-off');
+        $('#onDelivery').addClass('display-off');
+        $('#pastDelivery').removeClass('display-off');
+        $('#past-delivery-table').empty();
+
+    });
+    $('#new-tab').trigger('click');
+
+
+});
--- views/delivery/view-delivery.php
+++ views/delivery/view-delivery.php
@@ -19,116 +19,117 @@
     <h1 class="heading">System <span>Products</span></h1>
     <div class="container-core delivery-core ">
         <ul class="tabs">
-            <li><button class="btn-tab">New</button></li>
-            <li><button class="btn-tab">Current</button></li>
-            <li><button class="btn-tab">Completed</button></li>
+            <li><a id="new-tab" class="btn-tab">New</a></li>
+            <li><a id="on-tab" class="btn-tab">On-Going</a></li>
+            <li><a id="past-tab" class="btn-tab">Past</a></li>
         </ul>
         <div class = "tab-content">
             <div id="new" data-tab-content class="active">
-                <table class="table-scroll small-first-col">
+                <table id="newDelivery" class="table-scroll small-first-col ">
                     <thead>
                     <tr>
                         <th>Order ID</th>
                         <th>Time Stamp</th>
                         <th>Recipient Name</th>
+                        <th>Note</th>
                         <th>Recipient Contact</th>
                         <th>Delivery Cost</th>
                         <th>Total</th>
                         <th>View</th>
                     </tr>
                     </thead>
-                    <tbody class="body-half-screen">
-                    <tr>
-                        <td>1</td>
-                        <td>2021-10-23 14:30:00</td>
-                        <td>Kushan</td>
-                        <td>0785674532</td>
-                        <td>140</td>
-                        <td>4760</td>
-                        <td> <a class="order-view" href="/dashboard/delivery/viewnewdelivery"><i class="fas fa-arrow-circle-right"></i></a></td>
-                    </tr><tr>
-                        <td>1</td>
-                        <td>2021-10-23 14:30:00</td>
-                        <td>Kushan</td>
-                        <td>0785674532</td>
-                        <td>140</td>
-                        <td>4760</td>
-                        <td> <a class="order-view" href="/dashboard/delivery/viewnewdelivery"><i class="fas fa-arrow-circle-right"></i></a></td>
-                    </tr><tr>
-                        <td>1</td>
-                        <td>2021-10-23 14:30:00</td>
-                        <td>Kushan</td>
-                        <td>0785674532</td>
-                        <td>140</td>
-                        <td>4760</td>
-                        <td> <a class="order-view" href="/dashboard/delivery/viewnewdelivery"><i class="fas fa-arrow-circle-right"></i></a></td>
-                    </tr>
+                    <tbody id="new-delivery-table" class="body-half-screen clean-table">
+<!--                    <tr>-->
+<!--                        <td>1</td>-->
+<!--                        <td>2021-10-23 14:30:00</td>-->
+<!--                        <td>Kushan</td>-->
+<!--                        <td>0785674532</td>-->
+<!--                        <td>140</td>-->
+<!--                        <td>4760</td>-->
+<!--                        <td> <a class="order-view" href="/dashboard/delivery/viewnewdelivery"><i class="fas fa-arrow-circle-right"></i></a></td>-->
+<!--                    </tr><tr>-->
+<!--                        <td>1</td>-->
+<!--                        <td>2021-10-23 14:30:00</td>-->
+<!--                        <td>Kushan</td>-->
+<!--                        <td>0785674532</td>-->
+<!--                        <td>140</td>-->
+<!--                        <td>4760</td>-->
+<!--                        <td> <a class="order-view" href="/dashboard/delivery/viewnewdelivery"><i class="fas fa-arrow-circle-right"></i></a></td>-->
+<!--                    </tr><tr>-->
+<!--                        <td>1</td>-->
+<!--                        <td>2021-10-23 14:30:00</td>-->
+<!--                        <td>Kushan</td>-->
+<!--                        <td>0785674532</td>-->
+<!--                        <td>140</td>-->
+<!--                        <td>4760</td>-->
+<!--                        <td> <a class="order-view" href="/dashboard/delivery/viewnewdelivery"><i class="fas fa-arrow-circle-right"></i></a></td>-->
+<!--                    </tr>-->
                     </tbody>
                 </table>
 
             </div>
             <div id="ongoing" data-tab-content >
-<!--                <table class="table-scroll small-first-col">-->
-<!--                    <thead>-->
-<!--                    <tr>-->
-<!--                        <th></th>-->
-<!--                        <th>Delivery ID</th>-->
-<!--                        <th>Date</th>-->
-<!--                        <th>Time</th>-->
-<!--                        <th>Customer ID</th>-->
-<!--                        <th>Rider ID</th>-->
-<!--                        <th>Deliver Price(LKR)</th>-->
-<!--                        <th>No. Of Shop</th>-->
-<!--                        <th>View</th>-->
-<!--                    </tr>-->
-<!--                    </thead>-->
-<!--                    <tbody class="body-half-screen">-->
-<!--                    <tr>-->
-<!--                        <td>1.</td>-->
-<!--                        <td>D00011</td>-->
-<!--                        <td>21-10-2021</td>-->
-<!--                        <td>08.33 AM</td>-->
-<!--                        <td>C009894</td>-->
-<!--                        <td>R90466</td>-->
-<!--                        <td>300.00</td>-->
-<!--                        <td>2</td>-->
-<!--                        <td><a href="/dashboard/delivery/viewongoingdelivery"><button class="btn-item" type="submit"><span class="order-view"><i class="bx bx-show-alt"></i></span></button></a></td>-->
-<!--                    </tr>-->
-<!--                    </tbody>-->
-<!--                </table>-->
+                <table id="onDelivery" class="table-scroll small-first-col">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Delivery ID</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Customer ID</th>
+                        <th>Rider ID</th>
+                        <th>Deliver Price(LKR)</th>
+                        <th>No. Of Shop</th>
+                        <th>View</th>
+                    </tr>
+                    </thead>
+                    <tbody id = "on-delivery-table" class="body-half-screen clean-table">
+                    <tr>
+                        <td>1.</td>
+                        <td>D00011</td>
+                        <td>21-10-2021</td>
+                        <td>08.33 AM</td>
+                        <td>C009894</td>
+                        <td>R90466</td>
+                        <td>300.00</td>
+                        <td>2</td>
+                        <td><a href="/dashboard/delivery/viewongoingdelivery"><button class="btn-item" type="submit"><span class="order-view"><i class="bx bx-show-alt"></i></span></button></a></td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
             <div id="completed" data-tab-content >
-<!--                <table class="table-scroll small-first-col">-->
-<!--                    <thead>-->
-<!--                    <tr>-->
-<!--                        <th></th>-->
-<!--                        <th>Delivery ID</th>-->
-<!--                        <th>Date</th>-->
-<!--                        <th>Time</th>-->
-<!--                        <th>Customer ID</th>-->
-<!--                        <th>Rider ID</th>-->
-<!--                        <th>Deliver Price(LKR)</th>-->
-<!--                        <th>No. Of Shop</th>-->
-<!--                        <th>Finished Time</th>-->
-<!--                        <th>View</th>-->
-<!--                    </tr>-->
-<!--                    </thead>-->
-<!--                    <tbody class="body-half-screen">-->
-<!--                    <tr>-->
-<!--                        <td>1.</td>-->
-<!--                        <td>D00011</td>-->
-<!--                        <td>21-10-2021</td>-->
-<!--                        <td>08.33 AM</td>-->
-<!--                        <td>C009894</td>-->
-<!--                        <td>R90466</td>-->
-<!--                        <td>300.00</td>-->
-<!--                        <td>2</td>-->
-<!--                        <td>4.16 PM</td>-->
-<!--                        <td><a href="/dashboard/delivery/viewcompletedelivery"><button class="btn-item" type="submit"><span class="order-view"><i class="bx bx-show-alt"></i></span></button></a></td>-->
-<!--                    </tr>-->
-<!---->
-<!--                    </tbody>-->
-<!--                </table>-->
+                <table id="pastDelivery" class="table-scroll small-first-col">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Delivery ID</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Customer ID</th>
+                        <th>Rider ID</th>
+                        <th>Deliver Price(LKR)</th>
+                        <th>No. Of Shop</th>
+                        <th>Finished Time</th>
+                        <th>View</th>
+                    </tr>
+                    </thead>
+                    <tbody id="past-delivery-table" class="body-half-screen clean-table">
+                    <tr>
+                        <td>1.</td>
+                        <td>D00011</td>
+                        <td>21-10-2021</td>
+                        <td>08.33 AM</td>
+                        <td>C009894</td>
+                        <td>R90466</td>
+                        <td>300.00</td>
+                        <td>2</td>
+                        <td>4.16 PM</td>
+                        <td><a href="/dashboard/delivery/viewcompletedelivery"><button class="btn-item" type="submit"><span class="order-view"><i class="bx bx-show-alt"></i></span></button></a></td>
+                    </tr>
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
