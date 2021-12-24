ff02803f219e776d6e3ac3df65c6f7b069fc6292
Sanduni <2019cs154@stu.ucsc.lk>
2021-12-24 23:51:19 +0530

    remove hard code data in table body

--- views/shop/view-items.php
+++ views/shop/view-items.php
@@ -27,7 +27,7 @@
                 <th>Item Image</th>
                 <th>Item Name</th>
                 <th>Brand</th>
-<!--                <th>Unit</th>-->
+                <!--                <th>Unit</th>-->
                 <th>Unit Weight</th>
                 <th>MRP</th>
                 <th>Unit Price</th>
@@ -55,43 +55,6 @@
                                         <a href="#" data-toggle="tooltip" title="" class="btn btn-primary btn-red" data-original-title="Edit"><i class="far fa-trash-alt"></i></a>
                                     </td>
 
-                                </tr>
-                                <tr>
-                                    <td class="text-center"> <input type="checkbox" name="selected[]" value="110" checked="checked">
-                                    </td>
-                                    <td class="text-left">Veg110</td>
-                                    <td class="text-center"> <img src="https://essstr.blob.core.windows.net/essimg/350x/Small/Pic914039.jpg" alt="carrot" class="img-thumbnail"> </td>
-                                    <td class="text-left">Pumpkin</td>
-                                    <td class="text-left">Low-Country</td>
-                                    <td class="text-right"> <span>RS.50.00</span>
-                                    </td>
-                                    <td class="text-left">100 g</td>
-                                    <td class="text-right"> <span class="label label-success">0</span> </td>
-                                    <td class="text-left">Not Enable</td>
-                                    <td class="action-button">
-                                        <a href="#" data-toggle="tooltip" title="" class="btn btn-primary" data-original-title="Edit"><i class="far fa-edit"></i></a>
-                                        <a href="#" data-toggle="tooltip" title="" class="btn btn-primary btn-yellow" data-original-title="Edit"><i class="far fa-eye"></i></a>
-                                        <a href="#" data-toggle="tooltip" title="" class="btn btn-primary btn-red" data-original-title="Edit"><i class="far fa-trash-alt"></i></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="text-center"> <input type="checkbox" name="selected[]" value="2">
-                                    </td>
-                                    <td class="text-left">Veg002</td>
-                                    <td class="text-center"> <img src="https://essstr.blob.core.windows.net/essimg/350x/Small/Pic915013.jpg" alt="carrot" class="img-thumbnail"> </td>
-                                    <td class="text-left">Potato</td>
-                                    <td class="text-left">Up-Country</td>
-                                    <td class="text-right"> <span>RS.100.00</span>
-                                    </td>
-                                    <td class="text-left">100 g</td>
-                                    <td class="text-right"> <span class="label label-success">100 Kg</span> </td>
-                                    <td class="text-left">Enabled</td>
-                                    <td class="action-button">
-                                        <a href="#" data-toggle="tooltip" title="" class="btn btn-primary" data-original-title="Edit"><i class="far fa-edit"></i></a>
-                                        <a href="#" data-toggle="tooltip" title="" class="btn btn-primary btn-yellow" data-original-title="Edit"><i class="far fa-eye"></i></a>
-                                        <a href="#" data-toggle="tooltip" title="" class="btn btn-primary btn-red" data-original-title="Edit"><i class="far fa-trash-alt"></i></a>
-                                    </td>
-                                </tr>
 
             </tbody>
             <?php \app\core\form\Form::end(); ?>
