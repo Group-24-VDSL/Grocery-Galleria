14a775c323c1a9645ca832a44bbc20a480849aad
Sanduni <2019cs154@stu.ucsc.lk>
2021-12-26 17:48:27 +0530

    add rule to email(unique,email,required)

--- models/Shop.php
+++ models/Shop.php
@@ -57,6 +57,9 @@ class Shop extends UserModel
     {
         return [
             'ShopID'=>[self::RULE_UNIQUE],
+            'Email' => [self::RULE_EMAIL , self::RULE_REQUIRED, [self::RULE_UNIQUE, 'class' => self::class]],
+//            'Password' => [self::RULE_MIN,'min' => 8],
+//            'ConfirmPassword' => [self::RULE_MATCH,'match' => 'Password'],//check if the user class has the same email or not.
             'Name' => [self::RULE_REQUIRED],
             'Address' => [self::RULE_REQUIRED],
             'ContactNo' => [self::RULE_REQUIRED, self::RULE_PHONE],
