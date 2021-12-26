b6e80eb1778ec0822656450537a8e67409d345d0
Sanduni <2019cs154@stu.ucsc.lk>
2021-12-26 17:22:47 +0530

    add css file

--- views/layouts/dashboardL-shop.php
+++ views/layouts/dashboardL-shop.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="/css/all.css" />
     <link rel="stylesheet" href="/css/fonts.css" />
     <link rel="stylesheet" href="/css/dashboardStyle.css">
+    <link rel="stylesheet" href="/css/dashboardStyleStaff.css">
     <link rel="stylesheet" href="/css/template.css">
     <link href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css" rel="stylesheet"/>
     <script src="/js/jquery.min.js"></script>
