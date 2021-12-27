42f5cedfaa79762d0cec07dcb77ea4da7e524865
Sanduni <2019cs154@stu.ucsc.lk>
2021-12-27 12:53:46 +0530

    analytics function test

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -10,8 +10,19 @@ use app\models\Verification;
 class TestController extends Controller
 {
     public function test(Request $request){
-        $verify = Verification::findOne(['UniqueID' => $request->getBody()['id']]);
-        var_dump($verify);
-        var_dump($request->getBody());
+//        $verify = Verification::findOne(['UniqueID' => $request->getBody()['id']]);
+//        var_dump($verify);
+//        var_dump($request->getBody());
+
+        $this->setLayout('dashboardL-shop');
+        return $this->render('shop/shop-analytics');
+    }
+
+    public function shopOrderAnalytics(Request $request)
+    {
+
+//        $user = new Shop;
+        $this->setLayout('dashboardL-shop');
+        return $this->render('shop/shop-analytics');
     }
 }
