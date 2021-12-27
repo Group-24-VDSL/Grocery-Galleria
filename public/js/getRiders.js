bc0aacb3a63c2f89a9444defb20a486f80045044
Dilshan98-code <dilshanthenuka9@gmail.com>
2021-12-28 03:04:43 +0530

    get riders js.

---
+++ public/js/getRiders.js
@@ -0,0 +1,12 @@
+const getRidersBtn = document.getElementById('getRider-btn');
+
+// getRidersBtn.addEventListener('click',()=>{
+//     console.log('Hello');
+// })
+
+$(document).ready(function () {
+
+const ridersCity = $('#getRider-btn').data('city');
+const ridersSuburb = $('#getRider-btn').data('suburb');
+
+})
