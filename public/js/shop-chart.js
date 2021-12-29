498c064f84d90be2e5869e40f0535b7768799fa0
Sanduni <2019cs154@stu.ucsc.lk>
2021-12-29 21:43:17 +0530

    shop analytics js

--- public/js/shop-chart.js
+++ public/js/shop-chart.js
@@ -1,9 +1,10 @@
 const host = window.location.origin; //http://domainname
 
 //Api links
-
-const URLMonthOrders = host + "/api/getshoplastmonthorders" ;
 const URLMonthlyOrders = host + "/api/getshopmonthlyorders" ;
+const URLMonthOrders = host + "/api/getshoplastmonthorders" ;
+const URLMonthlyRevenues = host + "/api/getshoplastmonthlyrevenues" ;
+const URLMonthRevenues = host + "/api/getshoplastmonthrevenues" ;
 
 
 $( document ).ready(function() {
@@ -29,26 +30,37 @@ $( document ).ready(function() {
         document.getElementById("order-barchart-sum").innerHTML = sum;
 
         new Chart("orderBarchart", {
-            type: "bar",
+            // type: "bar",
             data: {
                 labels: xValues,
                 datasets: [{
+                    type: "bar",
                     label: 'Orders in month',
-                    backgroundColor: "green",
-                    borderColor: "green",
+                    backgroundColor: "#55a630ff",
+                    borderColor: "#55a630ff",
                     data: yValues
-                }]
+                },
+
+            ]
             },
             options: {
                 legend: {display: false},
 
-                scales: {},
+                scales: {
+                    yAxes: [{
+                        display:true,
+                        scaleLabel: {
+                            display: true,
+                           title: 'probability'
+                        }
+                    }]
+                },
 
                 plugins: {
                     title: {
                         display: true,
                         text: 'ORDERS IN LAST YEAR',
-                        color: 'green',
+                        color: '#55a630ff',
                         font: {
                             size: 14
                         }
@@ -86,7 +98,134 @@ $( document ).ready(function() {
             data: {
                 labels: xValues.reverse(),
                 datasets: [{
+                    lineTension: 0.3,
+                    borderWidth:1.5,
+                    strokeColor: "green",
+                    pointColor: "green",
+                    // .points[4].fillColor =  "red":"green",
+                    pointStrokeColor: "green",
                     label: 'Orders in day',
+                    data: yValues.reverse(),
+                    borderColor: "#55a630ff",
+                    fill: true
+                }]
+            },
+            options: {
+                legend: {display: false},
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'ORDERS IN LAST MONTH',
+                        color: '#55a630ff',
+                        layout: {
+                            padding: 5
+                        },
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            },
+        });
+
+    });
+
+    $.getJSON(URLMonthlyRevenues, function (orderlist) {
+        let xValues = [] ;
+        let yValues = [] ;
+
+        let a = Object.values(orderlist)[0]
+        console.log(a["NumberOfOrders"]);
+        console.log(Object.keys(orderlist).length)
+        for (let i = 0; i < Object.keys(orderlist).length; i++) {
+            if(Object.values(orderlist)[i]["Total"] == null){
+                yValues.push(0);
+            }
+            else {
+                yValues.push(parseInt(Object.values(orderlist)[i]["Total"]))
+            }
+
+        }
+        xValues = Object.keys(orderlist);
+
+        const orderAverage = yValues.reduce((a, b) => a + b) / yValues.length;
+        const sum = yValues.reduce((a, b) => {
+            return a + b;
+        });
+
+        document.getElementById("revenue-barchart-average").innerHTML = orderAverage.toFixed(2);
+        document.getElementById("revenue-barchart-sum").innerHTML = sum.toFixed(2);
+
+        new Chart("revenueBarchart", {
+            type: "bar",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    fill: true,
+                    backgroundColor: '#a6cee3',
+                    borderColor: '#a6cee3',
+                    label: 'Orders in month',
+                    backgroundColor: "green",
+                    borderColor: "green",
+                    data: yValues
+                }]
+            },
+            options: {
+                legend: {display: false},
+
+                scales: {},
+
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'ORDERS IN LAST YEAR',
+                        color: 'green',
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            }
+        });
+    });
+
+    $.getJSON(URLMonthRevenues, function (orders) {
+        let xValues = [];
+        let yValues = [];
+
+        let b = Object.values(orders)[0] ;
+
+        console.log(b["NumberOfOrders"]);
+        console.log(Object.keys(orders).length)
+
+        for (let i = 0; i < Object.keys(orders).length; i++) {
+            if(Object.values(orders)[i]["Total"] == null){
+                yValues.push(0);
+            }
+            else {
+                yValues.push(parseInt(Object.values(orders)[i]["Total"]))
+            }
+        }
+
+        xValues = Object.keys(orders);
+
+        const orderAverage = yValues.reduce((a, b) => a + b) / yValues.length;
+        const sum = yValues.reduce((a, b) => {
+            return a + b;
+        });
+
+        document.getElementById("revenue-linechart-average").innerHTML = orderAverage.toFixed(2);
+        document.getElementById("revenue-linechart-sum").innerHTML = sum.toFixed(2);
+
+        new Chart("revenueLinechart", {
+            type: "line",
+            data: {
+                labels: xValues.reverse(),
+                datasets: [{
+                    lineTension: 0.4,
+                    borderWidth:1.5,
+                    label: 'Orders in day',
+                    fill: true,
                     data: yValues.reverse(),
                     borderColor: "green",
                     fill: true
@@ -111,3 +250,4 @@ $( document ).ready(function() {
         });
     });
 });
+
