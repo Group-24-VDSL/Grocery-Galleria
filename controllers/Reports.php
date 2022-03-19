fd2c79870f36e9a84788b2df2cda239a42a7a951
Dilshan98-code <dilshanthenuka9@gmail.com>
2022-03-19 15:34:21 +0530

    Report controller done.

--- controllers/Reports.php
+++ controllers/Reports.php
@@ -16,18 +16,63 @@ class Reports extends Controller
 
     }
 
-//    public function itemReport()
-//    {
-//        $querySQL = "SELECT it.Name,it.UWeight, it.Unit, SUM(odc.Total) AS TotRev, sum(odc.Quantity) AS TotQty FROM item it
-//                    INNER JOIN ordercart odc ON
-//                        it.ItemID = odc.ItemID
-//                        INNER JOIN orders od ON
-//                        odc.CartID = od.CartID
-//                        WHERE MONTH(od.OrderDate) = 12
-//                        GROUP BY it.ItemID";
-//        $itemRevenue = DBModel::query($querySQL, fetch_type: \PDO::FETCH_ASSOC, fetchAll: true);
-//        return json_encode($itemRevenue);
-//    }
+    public function shopReports()
+    {
+        $this->setLayout('dashboardL-staff');
+        return $this->render("staff/shop-report");
+
+    }
+
+
+    public function productReports()
+    {
+        $this->setLayout('dashboardL-staff');
+        return $this->render("staff/product-report");
+    }
+
+    public function itemReport(Request $request)
+    {
+        $body = $request->getBody();
+        $itemID = $body['ItemID'];
+        $querySQL = "SELECT MONTHNAME(od.OrderDate) AS Month,SUM(odc.Quantity) AS Qty, SUM(odc.Total) AS TotSales FROM ordercart odc 
+                        INNER JOIN orders od ON
+                        odc.CartID = od.CartID
+                        WHERE odc.ItemID = $itemID
+                        GROUP BY MONTH(od.OrderDate)";
+        $itemRevenue = DBModel::query($querySQL, fetch_type: \PDO::FETCH_ASSOC, fetchAll: true);
+        return json_encode($itemRevenue);
+
+    }
+
+    public function getItemWeekReport(Request $request)
+    {
+        $body = $request->getBody();
+        $ID = $body['ItemID'];
+        $Date = $body['SalesDate'];
+        $querySQL = "SELECT DAYNAME(od.OrderDate) AS Day,SUM(odc.Quantity) AS Qty, SUM(odc.Total) AS Tot FROM ordercart odc 
+                    INNER JOIN orders od ON
+                    odc.CartID = od.CartID
+                    WHERE odc.ItemID = $ID AND WEEK(od.OrderDate) = WEEK('$Date')
+                    GROUP BY DAYNAME(od.OrderDate)";
+        $results = DBModel::query($querySQL,fetch_type:\PDO::FETCH_ASSOC,fetchAll: true);
+        return json_encode($results);
+
+    }
+    public function getTotalOrders()
+    {
+        $querySQL = "SELECT COUNT(DISTINCT OrderID) AS TotOrders,SUM(TotalCost) AS TotalRevenue,SUM(DeliveryCost) AS TotalDelReveneu FROM `orders`";
+        $result = DBModel::query($querySQL,fetch_type: \PDO::FETCH_ASSOC,fetchAll: true);
+        return json_encode($result);
+        
+    }
+
+    public function getTotalUsers()
+    {
+        $querySQL = "SELECT Role, COUNT(DISTINCT UserID) as roleCount FROM `login` WHERE Role='Shop' OR Role='Delivery' GROUP BY Role ORDER BY Role ASC";
+        $result =DBModel::query($querySQL,fetch_type: \PDO::FETCH_ASSOC,fetchAll: true);
+        return json_encode($result);
+        
+    }
 
     public function salesReportCurrent()
     {
@@ -53,9 +98,59 @@ class Reports extends Controller
 
     }
 
-    public function shopReport()
+    public function shopsReportMonthly(Request $request)
     {
+        $body =$request->getBody();
+        $cate = $body['Category'];
+        $querySQL = "SELECT s.ShopName,s.Address,s.ContactNo,s.Email,SUM(sod.ShopTotal) AS shopTot FROM shop s
+                        INNER JOIN shoporder sod ON
+                        s.ShopID = sod.ShopID
+                        WHERE s.Category=$cate AND 
+                        MONTH(sod.Date) = MONTH(CURRENT_DATE)
+                        GROUP BY s.ShopID 
+                        ORDER BY SUM(sod.ShopTotal) DESC
+                        LIMIT 10";
+        $topTenShopsOfMonth = DBModel::query($querySQL,fetch_type: \PDO::FETCH_ASSOC,fetchAll: true);
+        return json_encode($topTenShopsOfMonth);
 
+    }
+
+    public function shopsReportYearly(Request $request)
+    {
+        $body = $request->getBody();
+        $cate = $body['Category'];
+        $querySQL = "SELECT s.ShopName,s.Address,s.ContactNo,s.Email, SUM(sod.ShopTotal) AS shopTot FROM shop s
+                        INNER JOIN shoporder sod ON
+                        s.ShopID = sod.ShopID
+                        WHERE s.Category=$cate AND 
+                        YEAR(sod.Date) = YEAR(CURRENT_DATE)
+                        GROUP BY s.ShopID 
+                        ORDER BY SUM(sod.ShopTotal) DESC
+                        LIMIT 10";
+        $topShopsAllTime = DBModel::query($querySQL,fetch_type: \PDO::FETCH_ASSOC,fetchAll: true);
+        return json_encode($topShopsAllTime);
+
+    }
+
+    public function dailyRevenue(Request $request)
+    {
+        $body = $request->getBody();
+        $Date = $body['SalesDate'];
+        $querySQL = "SELECT HOUR(TIME(OrderDate)) AS timeInterval,SUM(TotalCost) AS totRevenue FROM `orders` WHERE DATE(OrderDate)='$Date'
+                    GROUP BY HOUR(TIME(OrderDate))";
+        $result = DBModel::query($querySQL,fetch_type: \PDO::FETCH_ASSOC,fetchAll: true);
+        return json_encode($result);
+
+    }
+
+    public function dailyTotOrders(Request $request)
+    {
+        $body = $request->getBody();
+        $Date = $body['SalesDate'];
+        $querySQl = "SELECT HOUR(TIME(OrderDate)) AS timeInterval,COUNT(DISTINCT OrderID) AS totOrders FROM `orders` WHERE DATE(OrderDate)='$Date'
+GROUP BY HOUR(TIME(OrderDate))";
+        $result =DBModel::query($querySQl,fetch_type: \PDO::FETCH_ASSOC,fetchAll: true);
+        return json_encode($result);
     }
 
 }
