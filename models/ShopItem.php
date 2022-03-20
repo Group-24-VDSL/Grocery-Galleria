4f981ef32e4c154cd85c8fa1ece0dd94948c7916
Dilshan98-code <dilshanthenuka9@gmail.com>
2022-03-20 16:39:27 +0530

    Shop item rules updated.

--- models/ShopItem.php
+++ models/ShopItem.php
@@ -47,7 +47,8 @@ class ShopItem extends DBModel
     {
         return [
             'ShopID'=> [self::RULE_REQUIRED],
-            'UnitPrice' => [self::RULE_REQUIRED,self::RULE_INT,[self::RULE_MIN_VAL,'minValue'=>0],[self::RULE_MAX_VAL_CLASS,'class'=> Item::class,'checkattribute'=>'MRP','where'=>'ItemID']],
+            'UnitPrice' => [self::RULE_REQUIRED,self::RULE_INT,[self::RULE_MIN_VAL,'minValue'=>0],
+                [self::RULE_IF_ONLY_THEN,'class'=> Item::class,'ValMatch'=>2,'CheckAttribute1'=>'MRP','matchAttribute1'=>'Category','where'=>'ItemID']],
             'Stock' => [self::RULE_REQUIRED,self::RULE_INT,[self::RULE_MIN_VAL,'minValue'=>5]]
         ];
     }
