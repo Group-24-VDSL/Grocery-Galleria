f05d97fc35f8e2b75630f87ed53b6e8ad6d24976
Wathsala Vishwajith <wathsala-vishwajith@github.com>
2022-03-21 21:24:53 +0530

    Database from sanduni

--- DBbackup/ggproject_complete.sql
+++ DBbackup/ggproject_complete.sql
@@ -9,7 +9,8 @@
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET
+time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -29,25 +30,13 @@ USE `ggproject`;
 -- Table structure for table `cart`
 --
 
-CREATE TABLE `cart` (
-  `CartID` int(11) NOT NULL,
-  `CustomerID` int(11) NOT NULL,
-  `Address` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `DateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE `cart`
+(
+    `CartID`     int(11) NOT NULL,
+    `Date`       date NOT NULL,
+    `Time`       time NOT NULL,
+    `CustomerID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
---
--- Truncate table before insert `cart`
---
-
-TRUNCATE TABLE `cart`;
---
--- Dumping data for table `cart`
---
-
-INSERT INTO `cart` (`CartID`, `CustomerID`, `Address`, `DateTime`) VALUES
-(1, 7, 'Address eka', '2021-12-04 14:52:46'),
-(2, 2, '', '2021-12-27 19:32:18');
 
 -- --------------------------------------------------------
 
@@ -55,22 +44,23 @@ INSERT INTO `cart` (`CartID`, `CustomerID`, `Address`, `DateTime`) VALUES
 -- Table structure for table `complaint`
 --
 
-CREATE TABLE `complaint` (
-  `ComplaintID` int(11) NOT NULL,
-  `CustomerID` int(11) NOT NULL,
-  `StaffID` int(11) NOT NULL,
-  `OrderID` int(11) NOT NULL,
-  `Nature` varchar(555) COLLATE utf8mb4_bin NOT NULL,
-  `OrderDate` date NOT NULL,
-  `ComplaintDate` date NOT NULL,
-  `SpecialDetails` varchar(500) COLLATE utf8mb4_bin NOT NULL,
-  `Prority` tinyint(1) NOT NULL,
-  `Regarding` tinyint(1) NOT NULL,
-  `Status` tinyint(1) NOT NULL
+CREATE TABLE `complaint`
+(
+    `ComplaintID`    int(11) NOT NULL,
+    `CustomerID`     int(11) NOT NULL,
+    `StaffID`        int(11) NOT NULL,
+    `OrderID`        int(11) NOT NULL,
+    `Nature`         varchar(555) COLLATE utf8mb4_bin NOT NULL,
+    `OrderDate`      text COLLATE utf8mb4_bin         NOT NULL,
+    `ComplaintDate`  text COLLATE utf8mb4_bin         NOT NULL,
+    `SpecialDetails` varchar(500) COLLATE utf8mb4_bin NOT NULL,
+    `Priority`       tinyint(1) NOT NULL,
+    `Regarding`      tinyint(1) NOT NULL,
+    `Status`         tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
--- Truncate table before insert `complaint`
+-- Dumping data for table `complaint`
 --
 
 TRUNCATE TABLE `complaint`;
@@ -80,16 +70,17 @@ TRUNCATE TABLE `complaint`;
 -- Table structure for table `customer`
 --
 
-CREATE TABLE `customer` (
-  `CustomerID` int(11) NOT NULL,
-  `Name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `Address` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `Email` varchar(55) COLLATE utf8mb4_bin NOT NULL,
-  `ContactNo` varchar(25) COLLATE utf8mb4_bin NOT NULL,
-  `City` varchar(55) COLLATE utf8mb4_bin NOT NULL,
-  `Suburb` varchar(55) COLLATE utf8mb4_bin NOT NULL,
-  `Location` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `PlaceID` varchar(1000) COLLATE utf8mb4_bin DEFAULT NULL
+CREATE TABLE `customer`
+(
+    `CustomerID` int(11) NOT NULL,
+    `Name`       varchar(255) COLLATE utf8mb4_bin NOT NULL,
+    `Address`    varchar(255) COLLATE utf8mb4_bin NOT NULL,
+    `Email`      varchar(55) COLLATE utf8mb4_bin  NOT NULL,
+    `ContactNo`  varchar(25) COLLATE utf8mb4_bin  NOT NULL,
+    `City`       varchar(55) COLLATE utf8mb4_bin  NOT NULL,
+    `Suburb`     varchar(55) COLLATE utf8mb4_bin  NOT NULL,
+    `Location`   varchar(255) COLLATE utf8mb4_bin NOT NULL,
+    `PlaceID`    varchar(1000) COLLATE utf8mb4_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
@@ -445,7 +436,7 @@ INSERT INTO `shop` (`ShopID`, `Name`, `Address`, `Email`, `ContactNo`, `City`, `
 (15, 'Saantha Pansilu', '45 Jambugasmulla Rd, Nugegoda', 'shop5@gmail.com', '+94772365985', 0, 8, 'Geenath Traders', 'Vegetable Importers', 1, '{&quot;lat&quot;:6.8728644421745475,&quot;lng&quot;:79.89126756305694}', 'ChIJuyUCVUFa4joRg9mBFNK_wy4'),
 (16, 'Kamala Geegana', '1 Edirigoda Rd, Nugegoda', 'shop6@gmail.com', '+94772344582', 0, 8, 'Kamala Enterprises', 'Fresh Vegetables', 1, '{&quot;lat&quot;:6.873250567593335,&quot;lng&quot;:79.89270522708892}', 'ChIJF1avXUFa4joR_25N3N_SnyM'),
 (17, 'Thisara Prabudda', '107/1/1,Stanley Thilakarathna Mawatha,Nugegoda', 'shop7@gmail.com', '+9477556982', 0, 8, 'Nugegoda Fruit Shop', 'All Kinds of Fruits', 4, '{&quot;lat&quot;:6.872374461881273,&quot;lng&quot;:79.89076598997116}', 'ChIJgXatAkRa4joRcYX_75Xt09U'),
-(18, 'Falil Ahmed', '15/4,St Joseph\'s Rd,Nugegoda', 'shop8@gmail.com', '+9471300982', 0, 8, 'Nugegoda Fruit Shop', 'Fruit Importers and Distributors', 4, '{&quot;lat&quot;:6.86985331888307,&quot;lng&quot;:79.89195554966926}', 'ChIJuW2he0Na4joROPealbMO0_k'),
+(18, 'Falil Ahmed', '15/4,St Joseph Rd,Nugegoda', 'shop8@gmail.com', '+9471300982', 0, 8, 'Nugegoda Fruit Shop', 'Fruit Importers and Distributors', 4, '{&quot;lat&quot;:6.86985331888307,&quot;lng&quot;:79.89195554966926}', 'ChIJuW2he0Na4joROPealbMO0_k'),
 (19, 'Gagana Randeepa', '107,Samudradevi Rd, Nugegoda', 'shop9@gmail.com', '+9470006982', 0, 8, 'Meegamu Fish', 'All Kinds of Fish', 3, '{&quot;lat&quot;:6.870877888510723,&quot;lng&quot;:79.89095106239319}', 'ChIJ3brXfURa4joRT3lK547-66E'),
 (20, 'Jalitha Kumara', '15/4,Stanley Tilakaratne Mawatha, Nugegoda', 'shop10@gmail.com', '+9478222582', 0, 8, 'Ceylon Fisheries', 'Fish Importers and Distributors', 3, '{&quot;lat&quot;:6.871463736298677,&quot;lng&quot;:79.88989427204132}', 'ChIJEbwlPkRa4joRjjCy7M_tmOs');
 
@@ -455,12 +446,15 @@ INSERT INTO `shop` (`ShopID`, `Name`, `Address`, `Email`, `ContactNo`, `City`, `
 -- Table structure for table `shopitem`
 --
 
-CREATE TABLE `shopitem` (
-  `ItemID` int(11) NOT NULL,
-  `ShopID` int(11) NOT NULL,
-  `UnitPrice` double NOT NULL,
-  `Stock` float NOT NULL,
-  `Enabled` tinyint(1) NOT NULL DEFAULT '1'
+CREATE TABLE `shopitem`
+(
+    `ItemID`      int(11) NOT NULL,
+    `ShopID`      int(11) NOT NULL,
+    `UnitPrice`   double NOT NULL,
+    `Stock`       float  NOT NULL,
+    `MaxLeadTime` int(11) NOT NULL,
+    `MinLeadTime` int(11) NOT NULL,
+    `Enabled`     tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
@@ -472,18 +466,55 @@ TRUNCATE TABLE `shopitem`;
 -- Dumping data for table `shopitem`
 --
 
-INSERT INTO `shopitem` (`ItemID`, `ShopID`, `UnitPrice`, `Stock`, `Enabled`) VALUES
-(1, 5, 280, 20, 0),
-(2, 5, 100, 18, 0),
-(3, 5, 150, 16, 1),
-(4, 5, 380, 10, 1),
-(5, 5, 190, 5, 0),
-(6, 5, 380, 20, 1),
-(7, 5, 110, 188, 1),
-(8, 5, 300, 230, 1),
-(11, 4, 125, 150, 0),
-(12, 4, 142, 49, 1),
-(13, 4, 112, 79, 1);
+INSERT INTO `shopitem` (`ItemID`, `ShopID`, `UnitPrice`, `Stock`, `MaxLeadTime`, `MinLeadTime`, `Enabled`)
+VALUES (1, 5, 206, 15, 1, 1, 1),
+       (2, 5, 100, 1000, 1, 1, 1),
+       (3, 1, 50, 20, 0, 0, 1),
+       (3, 5, 100, 45, 1, 1, 1),
+       (4, 1, 100, 40, 0, 0, 1),
+       (4, 5, 200, 10, 1, 1, 1),
+       (5, 1, 100, 90, 0, 0, 0),
+       (5, 5, 100, 70, 1, 1, 1),
+       (6, 5, 380, 45, 1, 1, 1),
+       (7, 1, 10, 23, 0, 0, 1),
+       (7, 5, 100, 30, 1, 1, 1),
+       (8, 1, 67, 245, 0, 0, 1),
+       (8, 5, 100, 50, 1, 1, 1),
+       (9, 5, 56, 120, 1, 1, 1),
+       (10, 1, 40, 100, 0, 0, 1),
+       (10, 5, 40, 100, 5, 2, 1),
+       (11, 5, 40, 100, 5, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shopitemsales`
+--
+
+CREATE TABLE `shopitemsales`
+(
+    `ItemID`   int(11) NOT NULL,
+    `ShopID`   int(11) NOT NULL,
+    `Quantity` int(11) NOT NULL,
+    `Date`     date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `shopitemsales`
+--
+
+INSERT INTO `shopitemsales` (`ItemID`, `ShopID`, `Quantity`, `Date`)
+VALUES (4, 5, 30, ''2020 - 12 - 12 ''),
+       (5, 5, 80, ''2021 - 08 - 12 ''),
+       (4, 5, 60, ''2021 - 10 - 14 ''),
+       (4, 5, 30, ''2020 - 12 - 12 ''),
+       (5, 5, 80, ''2021 - 08 - 12 ''),
+       (7, 1, 40, ''2021 - 10 - 14 ''),
+       (4, 5, 100, ''2022 - 02 - 18 ''),
+       (4, 5, 50, ''2021 - 12 - 08 ''),
+       (4, 5, 500, ''2021 - 10 - 14 ''),
+       (4, 5, 400, ''2021 - 12 - 08 ''),
+       (1, 5, 500, ''2021 - 12 - 15 '');
 
 -- --------------------------------------------------------
 
@@ -491,11 +522,14 @@ INSERT INTO `shopitem` (`ItemID`, `ShopID`, `UnitPrice`, `Stock`, `Enabled`) VAL
 -- Table structure for table `shoporder`
 --
 
-CREATE TABLE `shoporder` (
-  `ShopID` int(11) NOT NULL,
-  `CartID` int(11) NOT NULL,
-  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `ShopTotal` double NOT NULL
+CREATE TABLE `shoporder`
+(
+    `ShopID`       int(11) NOT NULL,
+    `CartID`       int(11) NOT NULL,
+    `Date`         date  NOT NULL,
+    `ShopTotal`    float NOT NULL,
+    `Status`       int(11) NOT NULL,
+    `CompleteDate` date  NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
@@ -507,11 +541,33 @@ TRUNCATE TABLE `shoporder`;
 -- Dumping data for table `shoporder`
 --
 
-INSERT INTO `shoporder` (`ShopID`, `CartID`, `Date`, `ShopTotal`) VALUES
-(4, 1, '2021-12-04 00:00:00', 7100),
-(4, 2, '2021-12-28 00:00:00', 1374),
-(5, 1, '2021-12-04 00:00:00', 7100),
-(5, 2, '2021-12-28 00:00:00', 1020);
+INSERT INTO `shoporder` (`ShopID`, `CartID`, `Date`, `ShopTotal`, `Status`, `CompleteDate`)
+VALUES (1, 1, ''2020 - 12 - 13 '', 2000, 1, ''0000 - 00 - 00 ''),
+       (1, 2, ''2021 - 12 - 11 '', 3000, 1, ''0000 - 00 - 00 ''),
+       (1, 3, ''2021 - 10 - 14 '', 2000, 1, ''2022 - 03 - 21 ''),
+       (1, 5, ''2021 - 12 - 11 '', 1000, 1, ''0000 - 00 - 00 ''),
+       (1, 6, ''2021 - 12 - 12 '', 1000, 1, ''2022 - 03 - 21 ''),
+       (1, 20, ''2021 - 11 - 25 '', 4000, 0, ''2021 - 12 - 24 ''),
+       (1, 22, ''2021 - 11 - 27 '', 4000, 0, ''2021 - 12 - 24 ''),
+       (1, 23, ''2021 - 04 - 27 '', 4000, 0, ''2021 - 12 - 24 ''),
+       (1, 25, ''2021 - 10 - 20 '', 4000, 0, ''2021 - 12 - 24 ''),
+       (1, 26, ''2021 - 10 - 20 '', 4000, 0, ''2021 - 12 - 24 ''),
+       (1, 28, ''2021 - 11 - 28 '', 4000, 0, ''2021 - 12 - 24 ''),
+       (1, 29, ''2020 - 11 - 25 '', 4000, 0, ''2021 - 12 - 24 ''),
+       (1, 30, ''2021 - 12 - 20 '', 4000, 0, ''2021 - 12 - 24 ''),
+       (1, 31, ''2020 - 12 - 20 '', 4000, 0, ''2021 - 12 - 24 ''),
+       (1, 34, ''2021 - 10 - 20 '', 4000, 0, ''2021 - 12 - 24 ''),
+       (2, 2, ''2021 - 12 - 16 '', 3000, 0, ''0000 - 00 - 00 ''),
+       (2, 6, ''0000 - 00 - 00 '', 1000, 1, ''0000 - 00 - 00 ''),
+       (2, 16, ''2020 - 12 - 12 '', 2000, 0, ''0000 - 00 - 00 ''),
+       (2, 17, ''2021 - 12 - 12 '', 4000, 1, ''2021 - 12 - 13 ''),
+       (3, 2, ''2021 - 10 - 01 '', 200.8, 0, ''0000 - 00 - 00 ''),
+       (4, 2, ''2021 - 12 - 08 '', 4000, 1, ''0000 - 00 - 00 ''),
+       (5, 1, ''2021 - 12 - 12 '', 590, 1, ''2021 - 12 - 13 ''),
+       (5, 2, ''2020 - 12 - 12 '', 2000, 1, ''2021 - 12 - 14 ''),
+       (5, 4, ''2020 - 12 - 12 '', 2000, 1, ''2021 - 12 - 14 ''),
+       (5, 16, ''2021 - 08 - 12 '', 2000, 1, ''2021 - 12 - 14 ''),
+       (5, 17, ''2021 - 12 - 11 '', 4000, 1, ''2021 - 12 - 13 '');
 
 -- --------------------------------------------------------
 
@@ -527,11 +583,6 @@ CREATE TABLE `staff` (
   `Email` varchar(55) COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
---
--- Truncate table before insert `staff`
---
-
-TRUNCATE TABLE `staff`;
 --
 -- Dumping data for table `staff`
 --
@@ -649,7 +700,22 @@ ALTER TABLE `login`
 -- Indexes for table `ordercart`
 --
 ALTER TABLE `ordercart`
-  ADD PRIMARY KEY (`CartID`,`ShopID`,`ItemID`);
+    ADD PRIMARY KEY (`CartID`, `ShopID`, `ItemID`),
+  ADD KEY `ShopID` (`ShopID`,`CustomerID`) USING BTREE;
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+    ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `CartID` (`CartID`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+    ADD PRIMARY KEY (`PaymentID`),
+  ADD KEY `OrderID` (`OrderID`);
 
 --
 -- Indexes for table `shop`
@@ -658,8 +724,59 @@ ALTER TABLE `shop`
   ADD PRIMARY KEY (`ShopID`);
 
 --
+-- Indexes for table `shopitem`
+--
+ALTER TABLE `shopitem`
+    ADD PRIMARY KEY (`ItemID`, `ShopID`),
+  ADD KEY `ShopID` (`ShopID`);
+
+--
+-- Indexes for table `shoporder`
+--
+ALTER TABLE `shoporder`
+    ADD PRIMARY KEY (`ShopID`, `CartID`),
+  ADD KEY `CartID` (`CartID`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+    ADD PRIMARY KEY (`StaffID`);
+
+--
+-- Indexes for table `temporarycart`
+--
+ALTER TABLE `temporarycart`
+    ADD PRIMARY KEY (`ItemID`, `ShopID`, `CustomerID`);
+
+--
+-- Indexes for table `verification`
+--
+ALTER TABLE `verification`
+    ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `UniqueID` (`UniqueID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+    MODIFY `CartID` int (11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+    MODIFY `ComplaintID` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `delivery`
+--
+ALTER TABLE `delivery`
+    MODIFY `DeliveryID` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `item`
@@ -671,7 +788,46 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+    MODIFY `UserID` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+    MODIFY `OrderID` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+    MODIFY `PaymentID` int (11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `shop`
+--
+ALTER TABLE `shop`
+    MODIFY `ShopID` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+DELIMITER
+$$
+--
+-- Events
+--
+CREATE
+DEFINER=`sanduni`@`localhost` EVENT `itemSales` ON SCHEDULE EVERY 1 MONTH STARTS ''
+2021-12-29 02:40:36
+'' ENDS ''
+2023-01-28 02:40:36
+'' ON COMPLETION PRESERVE ENABLE DO INSERT INTO shopitemsales
+SELECT ordercart.ItemID, ordercart.ShopID, ordercart.Quantity y, shoporder.Date
+FROM ordercart
+         INNER JOIN shoporder ON ordercart.CartID = shoporder.CartID AND ordercart.ShopID = shoporder.ShopID
+WHERE shoporder.Date > DATE (DATE_SUB(now()
+    , INTERVAL DAYOFMONTH(now())-0 DAY)- INTERVAL 12 MONTH)
+  AND shoporder.Date
+    < DATE (DATE_SUB(now()
+    , INTERVAL DAYOFMONTH(now())-1 DAY))$$
+    DELIMITER;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
