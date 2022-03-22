b360990bd86db41999a81c177a8fa3314fa887a5
Dilshan98-code <dilshanthenuka9@gmail.com>
2022-03-22 12:34:11 +0530

    staff order render done.

--- views/staff/view-orders.php
+++ views/staff/view-orders.php
@@ -10,54 +10,39 @@
     <h1 class="heading">System <span>Orders</span></h1>
     <div class="tabs">
         <ul class="order-tabs">
-            <li><a data-name="newOrders" id="new-tab" class="btn-tab">New</a></li>
-            <li><a data-name="onOrders" id="on-tab" class="btn-tab">On-Going</a></li>
-            <li><a data-name="pastOrders" id="past-tab" class="btn-tab">Past</a></li>
+            <li><a data-href="neworders" id="new-tab" class="btn-tab">New</a></li>
+            <li><a data-href="onorders" id="on-tab" class="btn-tab">On-Going</a></li>
+            <li><a data-href="pastorders" id="past-tab" class="btn-tab">Past</a></li>
         </ul>
-
         <div class="tab-content">
-
-            <div class="container-items">
-                <div class="table-header">
-
-                    <ul style="padding-right: 20px">
-                        <li>Order Id</li>
-                        <li>Order Date</li>
-                        <li>Order Time</li>
-                        <li>Customer ID</li>
-                        <li>No. of Shops</li>
-                        <li>Total (LKR)</li>
-                        <li>City</li>
-                        <li>Suburb</li>
-                        <li>View</li>
-                    </ul>
-                </div>
-                <div style="height: 190px" class="ongoing-items scroller ">
-                    <ul class="item">
-                        <li class="order-id">
-                            1230
-                        </li>
-                        <li class="order-date">27/09/2021</li>
-                        <li class="order-time">08:00 AM</li>
-                        <li class="customer-id">1102922</li>
-                        <li class="shop-no">4</li>
-                        <li class="order-total">2500.00</li>
-                        <li>Colombo</li>
-                        <li>Nugegoda</li>
-                        <li class="ubutton">
-                            <button class="btn-item" type="submit"><span class="order-view"><i
-                                            class='bx bx-show-alt'></i></span></button>
-                        </li>
-                    </ul>
-
-                </div>
+            <div id="new" data-tab-content class="active">
+                <table id="Orders" class="">
+                    <thead>
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Date Time</th>
+                        <th>Customer Name</th>
+                        <th>Note</th>
+                        <th>Customer Contact</th>
+                        <th>Rider Id</th>
+                        <th>Rider Name</th>
+                        <th>Rider Contact</th>
+                        <th>Delivery Cost</th>
+                        <th>Total</th>
+                        <th>View</th>
+                    </tr>
+                    </thead>
+                    <tbody id="Order-info-rows">
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+
 </div>
 <div class="core">
     <h1 class="heading">Add <span>Complaint</span></h1>
 </div>
-<script src="/js/staff-order.js" defer></script>
+<script src="/js/staff-order.js"></script>
 
 
