8ae6a04deee37a9f49e8bf1c42618d0037e06490
Dilshan98-code <dilshanthenuka9@gmail.com>
2022-03-22 12:32:51 +0530

    order render done.

--- public/js/staff.js
+++ public/js/staff.js
@@ -27,214 +27,221 @@ const URLItemsAPI = host + "/api/items";
 const URLItemAPI = host + "/api/item";
 const ItemTable = document.getElementById('item-table');
 const URLItemReport = host + '/dashboard/staff/itemreport';
-const URLItemWeekReport = host+'/dashboard/staff/getitemweekreport';
+const URLItemWeekReport = host + '/dashboard/staff/getitemweekreport';
 
-$(".btn-tab").click(function () {
-    $('#item-table').empty();
-    let URLFindItems = URLItemsAPI.concat($(this).data("href"));
-    $.getJSON(URLFindItems, function (Items) {
-        Items.forEach(Item => {
-                const itemRow = document.createElement('ul');
-                itemRow.classList.add('row');
+$(document).ready(function () {
+    $(".btn-tab").click(function () {
+        let table = $('#Items').DataTable();
+        table.destroy();
+        $('#Item-info-rows').empty();
+        let URLFindItems = URLItemsAPI.concat($(this).data("href"));
+        $.getJSON(URLFindItems, function (Items) {
+            Items.forEach(Item => {
+                const itemRow = document.createElement('tr');
                 itemRow.innerHTML = `
-                 <li class="row-img">
+                 <td class="row-img">
                     <img src="${Item.ItemImage}" alt="${Item.Name}" />
-                </li>
-                <li class="row-name">${Item.Name}</li>
-                <li class="row-brand">${Item.Brand}</li>
-                <li class="row-unit">${Item.Unit}</li>
-                <li class="row-minWeight">${Item.UWeight}</li>
-                <li class="row-mrp">${Item.MRP}</li>
-                <li class="row-IncStep">${Item.MaxCount}</li>
-                <li class="row-ubutton">
+                </td>
+                <td class="row-name">${Item.Name}</td>
+                <td class="row-brand">${Item.Brand}</td>
+                <td class="row-unit">${Item.Unit}</td>
+                <td class="row-minWeight">${Item.UWeight}</td>
+                <td class="row-mrp">${Item.MRP}</td>
+                <td class="row-IncStep">${Item.MaxCount}</td>
+                <td class="row-ubutton">
                     <button id="ItemID=${Item.ItemID}" data-href="?ItemID=${Item.ItemID}" class="btn-row">Update</button>
-                </li>
+                </td>
                 `
-                ItemTable.appendChild(itemRow);
-            }
-        )
-    }).then(function () {
-        const itemBtns = document.getElementsByClassName('btn-row');
-        $(itemBtns).click(function () {
-            $('.chart-div').empty();
-            const div1 = document.createElement('div');
-            div1.classList.add('chart');
-            const canva1 = document.createElement('canvas');
-            canva1.id = 'myChart1';
-            div1.appendChild(canva1);
-            chartDiv1.appendChild(div1);
+                $('#Item-info-rows').append(itemRow);
+            })
+        }).then(function () {
+            $('#Items').DataTable({
+                "pageLength": 5,
+            });
+            ////
+            console.log("here")
+            const itemBtns = document.getElementsByClassName('btn-row');
+            $(itemBtns).click(function () {
+                $('.chart-div').empty();
+                const div1 = document.createElement('div');
+                div1.classList.add('chart');
+                const canva1 = document.createElement('canvas');
+                canva1.id = 'myChart1';
+                div1.appendChild(canva1);
+                chartDiv1.appendChild(div1);
 
-            const URLFindItem = URLItemAPI.concat($(this).data("href"));
-            const URLGetItemReport = URLItemReport.concat($(this).data("href"));
-            $("#storeItemID").val($(this).data("href"));
+                const URLFindItem = URLItemAPI.concat($(this).data("href"));
+                const URLGetItemReport = URLItemReport.concat($(this).data("href"));
+                $("#storeItemID").val($(this).data("href"));
 
-            // console.log(URLGetItemReport)
-            // $('.chart-div').empty();
-            let totQty1 = [];
-            let totSales1 = []
-            $.getJSON(URLGetItemReport, function (reportData) {
-                reportData.forEach(data => {
-                    totSales1.push({x: data.Month, y: data.TotSales});
-                    totQty1.push({x: data.Month, y: data.Qty});
-                });
-                const ctx = document.getElementById('myChart1').getContext('2d');
-                const labelsArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-                    'August', 'September', 'October', 'November', 'December'];
-                const data = {
-                    labels: labelsArr,
-                    datasets: [{
-                        label: 'Month Revenue',
-                        data: totSales1,
-                        backgroundColor: [
-                            '#06D6A0'
-                        ],
-                        borderColor: [
-                            'rgb(153, 102, 255)',
-
-                        ],
-                        borderWidth: 1
-
-                    },
-                        {
-                            label: 'Total Qty',
-                            data: totQty1,
+                // console.log(URLGetItemReport)
+                // $('.chart-div').empty();
+                let totQty1 = [];
+                let totSales1 = []
+                $.getJSON(URLGetItemReport, function (reportData) {
+                    reportData.forEach(data => {
+                        totSales1.push({x: data.Month, y: data.TotSales});
+                        totQty1.push({x: data.Month, y: data.Qty});
+                    });
+                    const ctx = document.getElementById('myChart1').getContext('2d');
+                    const labelsArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                        'August', 'September', 'October', 'November', 'December'];
+                    const data = {
+                        labels: labelsArr,
+                        datasets: [{
+                            label: 'Month Revenue',
+                            data: totSales1,
                             backgroundColor: [
-                                '#00bfff'
+                                '#06D6A0'
                             ],
                             borderColor: [
                                 'rgb(153, 102, 255)',
 
                             ],
                             borderWidth: 1
-                        }
-                    ],
-                };
-                const config = {
-                    type: 'bar',
-                    data: data,
-                    options: {
-                        responsive: true,
-                        plugins: {
-                            title: {
-                                display: true,
-                                text: 'Monthly Report of the Item'
+
+                        },
+                            {
+                                label: 'Total Qty',
+                                data: totQty1,
+                                backgroundColor: [
+                                    '#00bfff'
+                                ],
+                                borderColor: [
+                                    'rgb(153, 102, 255)',
+
+                                ],
+                                borderWidth: 1
+                            }
+                        ],
+                    };
+                    const config = {
+                        type: 'bar',
+                        data: data,
+                        options: {
+                            responsive: true,
+                            plugins: {
+                                title: {
+                                    display: true,
+                                    text: 'Monthly Report of the Item'
+                                }
                             }
                         }
                     }
-                }
-                const myChart = new Chart(ctx, config);
-            })
-            // console.log(URLFindItem);
-            $.getJSON(URLFindItem, function (Item) {
-                console.log(Item)
-                $('#item-data').empty().append(Item.Name + ' Report');
-                $('#ItemID').val(Item.ItemID);
-                $('#Category').val(Item.Category);
-                // console.log(document.getElementById('Category').value);
-                $('#Unit').val(Item.Unit);
-                // console.log(document.getElementById('Unit').value);
-                $('#Name').val(Item.Name);
-                // console.log(document.getElementById('Name').value);
-                $('#ImgDis').attr('src', Item.ItemImage);
-                // console.log(Item.ItemImage);
-                $('#Brand').val(Item.Brand);
-                // console.log(document.getElementById('Brand').value);
-                $('#UWeight').val(Item.UWeight);
-                // console.log(document.getElementById('UWeight').value);
-                $('#MaxCount').val(Item.MaxCount);
-                // console.log(document.getElementById('MaxCount').value);
-                $('#MRP').val(Item.MRP);
-                // console.log(document.getElementById('MRP').value);
-                $('#Status').val(Item.Status).checked();
+                    const myChart = new Chart(ctx, config);
+                }).then(function () {
+                    document.getElementById('SalesDate').addEventListener('change', (event) => {
+                        $('#chartDiv2').empty();
+                        const div2 = document.createElement('div');
+                        div2.classList.add('chart');
+                        const canva2 = document.createElement('canvas');
+                        canva2.id = 'myChart2';
+                        div2.appendChild(canva2);
+                        chartDiv2.appendChild(div2);
+                        const itemIDParam = $('#storeItemID').val();
+                        const URLGetWeekReport = URLItemWeekReport.concat(itemIDParam + '&SalesDate=' + event.target.value);
+                        console.log(URLGetWeekReport)
+                        let totQty2 = [];
+                        let totSales2 = []
+                        $.getJSON(URLGetWeekReport, function (weekData) {
+                            weekData.forEach(data => {
+                                totSales2.push({x: data.Day, y: data.Tot});
+                                totQty2.push({x: data.Day, y: data.Qty});
+                            });
+                            const ctx = document.getElementById('myChart2').getContext('2d');
+                            const labelsArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+                            const data = {
+                                labels: labelsArr,
+                                datasets: [{
+                                    label: 'Week Revenue',
+                                    data: totSales2,
+                                    backgroundColor: [
+                                        '#06D6A0'
+                                    ],
+                                    borderColor: [
+                                        'rgb(153, 102, 255)',
 
+                                    ],
+                                    borderWidth: 1
 
-            })
-        })
-    }).then(function () {
-        document.getElementById('SalesDate').addEventListener('change', (event) => {
-            $('#chartDiv2').empty();
-            const div2 = document.createElement('div');
-            div2.classList.add('chart');
-            const canva2 = document.createElement('canvas');
-            canva2.id = 'myChart2';
-            div2.appendChild(canva2);
-            chartDiv2.appendChild(div2);
-            const itemIDParam = $('#storeItemID').val();
-            const URLGetWeekReport = URLItemWeekReport.concat(itemIDParam+'&SalesDate='+event.target.value);
-            console.log(URLGetWeekReport)
-            let totQty2 = [];
-            let totSales2 = []
-            $.getJSON(URLGetWeekReport, function (weekData) {
-                weekData.forEach(data => {
-                    totSales2.push({x: data.Day, y: data.Tot});
-                    totQty2.push({x: data.Day, y: data.Qty});
-                });
-                const ctx = document.getElementById('myChart2').getContext('2d');
-                const labelsArr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'];
-                const data = {
-                    labels: labelsArr,
-                    datasets: [{
-                        label: 'Week Revenue',
-                        data: totSales2,
-                        backgroundColor: [
-                            '#06D6A0'
-                        ],
-                        borderColor: [
-                            'rgb(153, 102, 255)',
+                                },
+                                    {
+                                        label: 'Total Qty',
+                                        data: totQty2,
+                                        backgroundColor: [
+                                            '#00bfff'
+                                        ],
+                                        borderColor: [
+                                            'rgb(153, 102, 255)',
 
-                        ],
-                        borderWidth: 1
-
-                    },
-                        {
-                            label: 'Total Qty',
-                            data: totQty2,
-                            backgroundColor: [
-                                '#00bfff'
-                            ],
-                            borderColor: [
-                                'rgb(153, 102, 255)',
-
-                            ],
-                            borderWidth: 1
-                        }
-                    ],
-                };
-                const config = {
-                    type: 'bar',
-                    data: data,
-                    options: {
-                        responsive: true,
-                        plugins: {
-                            title: {
-                                display: true,
-                                text: 'Week Report of the Item'
+                                        ],
+                                        borderWidth: 1
+                                    }
+                                ],
+                            };
+                            const config = {
+                                type: 'bar',
+                                data: data,
+                                options: {
+                                    responsive: true,
+                                    plugins: {
+                                        title: {
+                                            display: true,
+                                            text: 'Week Report of the Item'
+                                        }
+                                    }
+                                }
                             }
-                        }
-                    }
-                }
-                const myChart = new Chart(ctx, config);
+                            const myChart = new Chart(ctx, config);
+                        })
+                    })
+                })
+                // console.log(URLFindItem);
+                $.getJSON(URLFindItem, function (Item) {
+                    console.log(Item)
+                    $('#item-data').empty().append(Item.Name + ' Report');
+                    $('#ItemID').val(Item.ItemID);
+                    $('#Category').val(Item.Category);
+                    // console.log(document.getElementById('Category').value);
+                    $('#Unit').val(Item.Unit);
+                    // console.log(document.getElementById('Unit').value);
+                    $('#Name').val(Item.Name);
+                    // console.log(document.getElementById('Name').value);
+                    $('#ImgDis').attr('src', Item.ItemImage);
+                    // console.log(Item.ItemImage);
+                    $('#Brand').val(Item.Brand);
+                    // console.log(document.getElementById('Brand').value);
+                    $('#UWeight').val(Item.UWeight);
+                    // console.log(document.getElementById('UWeight').value);
+                    $('#MaxCount').val(Item.MaxCount);
+                    // console.log(document.getElementById('MaxCount').value);
+                    $('#MRP').val(Item.MRP);
+                    // console.log(document.getElementById('MRP').value);
+                    $('#Status').val(Item.Status).checked();
+
+                })
             })
+
         })
     })
-})
-
-
-$('#btn-vege').trigger('click');
-
 // $('#Status').val(1);
-$('#Status').change(function (){
-    if($(this).is(':checked')){
-        $(this).val(1);
-        console.log($(this).val())
-        // console.log(document.getElementById('Status'))
-    }else{
-        $(this).val(0);
-        console.log($(this).val())
-        // console.log(document.getElementById('Status'))
+    $('#Status').change(function () {
+        if ($(this).is(':checked')) {
+            $(this).val(1);
+            console.log($(this).val())
+            // console.log(document.getElementById('Status'))
+        } else {
+            $(this).val(0);
+            console.log($(this).val())
+            // console.log(document.getElementById('Status'))
 
-    }
+        }
+    })
+
+    $('#veg-tab-items').trigger("click");
 })
+
+
 
 
 
