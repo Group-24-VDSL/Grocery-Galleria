60009fee3b253a120b3f6c90b9321840e24b1dd4
Sanduni <2019cs154@stu.ucsc.lk>
2022-03-24 11:23:56 +0530

    .

--- public/js/complaint.js
+++ public/js/complaint.js
@@ -1,21 +1,4 @@
 
-// const getUrlParameter = function getUrlParameter(sParam) {
-//   let sPageURL = window.location.search.substring(1),
-//       sURLVariables = sPageURL.split('&'),
-//       sParameterName,
-//       i;
-//
-//   for (i = 0; i < sURLVariables.length; i++) {
-//     sParameterName = sURLVariables[i].split('=');
-//
-//     if (sParameterName[0] === sParam) {
-//       return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-//     }
-//   }
-//   return false;
-// }
-// const host = window.location.origin; //http://domainname
-
 //Api links
 
 
