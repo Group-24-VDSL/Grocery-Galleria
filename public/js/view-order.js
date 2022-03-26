a323cdda24be37515f55a8a77fd01de090b0df9d
Dilshan98-code <dilshanthenuka9@gmail.com>
2022-03-26 14:10:04 +0530

    render shop and Customer locations.

--- public/js/view-order.js
+++ public/js/view-order.js
@@ -8,9 +8,7 @@ const category = ["Vegetable", "Fruit", "Grocery", "Fish", "Meat"]
 const UnitTag = ["Kg", "g", "L", "ml", "Unit"];
 const OrderState = ["New","OnGoing","Past"];
 const ordersDetailsDIv = document.getElementById('orderDetails');
-let map;
-let shopMarkers = [];
-let infoWindows = [];
+
 $(document).ready(function () {
     const orderID = $('#OrderID').val();
     const cartID = $('#CartID').val();
@@ -98,95 +96,7 @@ document.getElementById('orderStatus').innerHTML = `${OrderState[Status]}`
             })
         })
     })
-    // Create the script tag, set the appropriate attributes
-    let script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAwYJrYLyEaQGRUYEnh10GS5luyYnt2a5U&callback=initMap';
-    // script.src = 'https://maps.googleapis.com/maps/api/js?key=&callback=initMap';
-    script.async = true;
 
-
-    window.initMap = function () {
-
-        const geocoder = new google.maps.Geocoder();
-
-
-        let infoWindow = new google.maps.InfoWindow({
-            content: "Customer",
-        });
-
-        let customer = $(document.getElementById("map")).data('location');
-
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: customer,
-            zoom: 16,
-        });
-        let cus_icon = {
-            url: host + '/img/human_icon.png', // url
-            scaledSize: new google.maps.Size(25, 25), // scaled size
-            origin: new google.maps.Point(0, 0), // origin
-            anchor: new google.maps.Point(0, 0) // anchor
-        };
-        let myMarker = new google.maps.Marker({
-            position: new google.maps.LatLng(customer['lat'], customer['lng']),
-            icon: cus_icon,
-        });
-
-        myMarker.setMap(map);
-
-        myMarker.addListener("mouseover", () => {
-            infoWindow.open({
-                anchor: myMarker,
-                map,
-                shouldFocus: false,
-            });
-        });
-        myMarker.addListener("mouseout", () => {
-            infoWindow.close();
-        });
-
-    }
-
-
-    // Append the 'script' element to 'head'
-    document.head.appendChild(script);
-
-    const URLShopLocations = host + '/dashboard/staff/getshoplocations';
-
-    let shop_icon = {
-        url: host + '/img/shop_icon.png', // url
-        scaledSize: new google.maps.Size(25, 25), // scaled size
-        origin: new google.maps.Point(0, 0), // origin
-        anchor: new google.maps.Point(0, 0) // anchor
-    };
-    const URLGetShopLocations = URLShopLocations.concat("?CartID=" + cartID);
-    $.getJSON(URLGetShopLocations, function (shopLocations) {
-        console.log(URLGetShopLocations)
-        shopMarkers.forEach(shopMarker => {
-            shopMarker.setMap(null);
-        })
-        shopMarkers = [];
-        infoWindows = [];
-        shopLocations.forEach((shopLocation, i) => {
-            console.log(shopLocation)
-            shopMarkers[i] = new google.maps.Marker({
-                position: new google.maps.LatLng(shopLocation.lat, shopLocation.lng),
-                icon: shop_icon,
-            });
-        });
-
-        shopMarkers[i].setMap(map);
-
-        shopMarkers[i].addListener("mouseover", () => {
-            infoWindows[i].open({
-                anchor: shopMarkers[i],
-                map,
-                shouldFocus: false,
-            });
-        });
-        shopMarkers[i].addListener("mouseout", () => {
-            infoWindows[i].close();
-        });
-    });
 })
 
 
