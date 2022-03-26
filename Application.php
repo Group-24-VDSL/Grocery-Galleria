34c5c2aba6f2b03c7ef4d47e7689cf78b5ed0eab
Dilshan98-code <dilshanthenuka9@gmail.com>
2022-03-26 22:51:42 +0530

    last min fixes.

--- Application.php
+++ Application.php
@@ -290,10 +290,6 @@ class Application
         return self::$app->session->get('user');
     }
 
-    public static function getUserID(){
-        return self::$app->session->get('user');
-    }
-
 
     public static function getUserRole(){
         return self::$app->user->Role??null;
